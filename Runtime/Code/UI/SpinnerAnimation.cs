333a677a0052221599f19cb35e2f1d26a2f59beb
Luke Chatton <chattonluke@gmail.com>
2023-07-06 13:33:51 -0700

    fix spinner

---
+++ Runtime/Code/UI/SpinnerAnimation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerAnimation : MonoBehaviour
+{
+    private RectTransform rectComponent;
+    public float rotateSpeed = -150f;
+    
+    private void Start()
+    {
+        rectComponent = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        rectComponent.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+    }
+}
