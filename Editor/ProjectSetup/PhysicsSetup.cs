c4efabe623b3d91580fc83f37f910a260eb120d6
Luke Chatton <chattonluke@gmail.com>
2023-07-06 13:29:41 -0700

    add projectile system and physics setup

---
+++ Editor/ProjectSetup/PhysicsSetup.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PhysicsSetup : MonoBehaviour
+{
+    private void Start()
+    {
+        PhysicsLayerEditor.CreateLayer("Character");
+        PhysicsLayerEditor.CreateLayer("Block");
+        PhysicsLayerEditor.CreateLayer("BridgeAssist");
+        PhysicsLayerEditor.CreateLayer("GroundItem");
+        PhysicsLayerEditor.CreateLayer("FirstPerson");
+        PhysicsLayerEditor.CreateLayer("Projectile");
+    }
+}
