2f4260c1694e5b9e306bca982df297fef37efc37
Luke Chatton <chattonluke@gmail.com>
2023-07-06 15:33:09 -0700

    fix default prefabs

--- Editor/ProjectSetup/FishNetSetup.cs
+++ Editor/ProjectSetup/FishNetSetup.cs
@@ -1,11 +1,21 @@
 using System;
+using FishNet.Configuring;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class FishNetSetup : MonoBehaviour
 {
+    private void Awake()
+    {
+        FishNet.Configuring.Configuration.Configurations.PrefabGenerator.SearchScope =
+            (int)SearchScopeType.SpecificFolders;
+    }
+
     private void Start()
     {
+        FishNet.Configuring.Configuration.Configurations.PrefabGenerator.SearchScope =
+            (int)SearchScopeType.SpecificFolders;
+
         FishNet.Configuring.Configuration.Configurations.PrefabGenerator.DefaultPrefabObjectsPath =
             "Packages/gg.easy.airship/Runtime/Code/DefaultPrefabObjects.asset";
 
