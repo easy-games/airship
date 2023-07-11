5e8adad2d13203376f1d000702d112e38f7745b9
emoritz45 <47371461+emoritz45@users.noreply.github.com>
2023-07-11 11:25:51 -0500

    Remove large file

---
+++ Runtime/Code/Core/KeyValuePairs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.Code.Core
+{
+	[LuauAPI]
+	[Serializable]
+	public class KeyValuePairs
+	{
+		public List<SimpleKeyValuePair> KVPs;
+
+		public Dictionary<string, string> ToDictionary()
+		{
+			var dict = new Dictionary<string, string>();
+
+			if (KVPs != null)
+			{
+				foreach (var kv in KVPs)
+				{
+					dict.Add(kv.Key, kv.Value);
+				}
+			}
+
+			return dict;
+		}
+	}
+}
