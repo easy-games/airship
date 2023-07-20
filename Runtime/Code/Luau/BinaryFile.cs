010d0431edd8ee0fbc31844019c668c0919ecb83
Stephen Leitnick <sleitnick@gmail.com>
2023-07-20 11:56:08 -0400

    Fast lua import

--- Runtime/Code/Luau/BinaryFile.cs
+++ Runtime/Code/Luau/BinaryFile.cs
@@ -1,14 +1,11 @@
-
-using System;
 using UnityEngine;
-using UnityEditor;
 
 namespace Luau
 { 
     [System.Serializable]
     public class BinaryFile : ScriptableObject
     {
-        
+        [HideInInspector]
         public byte[] m_bytes;
         public bool m_compiled = false;
         [TextArea(15,20)]
