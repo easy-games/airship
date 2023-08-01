5d0b7a2162d7154cde2566f7eea54c997655c7d8
Tshadow0508 <tshadow05@gmail.com>
2023-08-01 14:45:29 -0700

    Moved entity spine clamping to CS

---
+++ Runtime/Code/Misc/MathUtil.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathUtil : MonoBehaviour
+{
+    public static float ClampAngle(float angle, float min, float max) {
+        float start = (min + max) * 0.5f - 180;
+        float floor = Mathf.FloorToInt((angle - start) / 360) * 360;
+        return Mathf.Clamp(angle, min + floor, max + floor);
+    }
+}
