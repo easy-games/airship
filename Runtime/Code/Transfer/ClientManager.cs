b3028f2030b7f46a2f2da577b12d89711a2379f5
Luke Chatton <chattonluke@gmail.com>
2023-08-19 18:12:32 -0700

    2% battery life

--- Runtime/Code/Transfer/ClientManager.cs
+++ Runtime/Code/Transfer/ClientManager.cs
@@ -1,11 +1,17 @@
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 [LuauAPI]
-public class TransferManager {
-    public static bool ConnectToServer(string address, ushort port) {
+public class TransferManager : Singleton<TransferManager> {
+    public bool ConnectToServer(string address, ushort port) {
+        StartCoroutine(address, port);
+        return true;
+    }
+
+    private IEnumerator StartTransfer(string address, ushort port) {
         CrossSceneState.ServerTransferData.address = address;
         CrossSceneState.ServerTransferData.port = port;
         SceneManager.LoadScene("CoreScene");
-        return true;
+        yield break;
     }
 }
