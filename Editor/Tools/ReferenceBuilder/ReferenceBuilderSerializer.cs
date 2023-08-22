75f8aa011d28d9e84cc3fee3356e8df611d89ccb
Tshadow0508 <tshadow05@gmail.com>
2023-08-22 16:08:39 -0700

    Updated reference manager path after folder restructure

--- Editor/Tools/ReferenceBuilder/ReferenceBuilderSerializer.cs
+++ Editor/Tools/ReferenceBuilder/ReferenceBuilderSerializer.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 namespace ReferenceBuilder{
     public class ReferenceBuilderSerializer : MonoBehaviour {
         private const string PathToCompiledScript
-            = "Typescript~/src/Shared/Util/ReferenceManagerResources.ts";
+            = "Bundles/Imports/Core/Core~/src/Shared/Util/ReferenceManagerResources.ts";
+        //Airship-Bedwars\Assets\Bundles\Imports\Core\Core~\src\Shared\Util\ReferenceManagerResources.ts
         private const string ConstantDeclarations
             = "export interface BundleGroup{ \n\t" +
               "id:BundleGroupNames; \n\t" +
