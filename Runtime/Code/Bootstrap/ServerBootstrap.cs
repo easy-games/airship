a0acb6b78b122504e666868a7f3b2e952ad9f270
Luke Chatton <chattonluke@gmail.com>
2023-08-24 11:11:50 -0700

    let's try this

--- Runtime/Code/Bootstrap/ServerBootstrap.cs
+++ Runtime/Code/Bootstrap/ServerBootstrap.cs
@@ -304,17 +304,11 @@ public class ServerBootstrap : MonoBehaviour
 
         print("[Server Bootstrap]: Loading game bundle: " + startupConfig.GameBundleId);
         yield return SystemRoot.Instance.LoadPackages(packages, SystemRoot.Instance.IsUsingBundles(editorConfig));
-        
-        Debug.Log("[Server Bootstrap]: Loading scene " + startupConfig.StartingSceneName + "...");
-
-        Debug.Log("All scenes:");
-        for (int i = 0; i < SceneManager.sceneCount; i++) {
-	        Debug.Log("  - " + SceneManager.GetSceneAt(i));
-        }
 
         var st = Stopwatch.StartNew();
 
         var scenePath = $"assets/bundles/shared/scenes/{startupConfig.StartingSceneName}";
+        Debug.Log("[Server Bootstrap]: Loading scene " + scenePath);
         var sceneLoadData = new SceneLoadData(scenePath);
         sceneLoadData.ReplaceScenes = ReplaceOption.None;
         InstanceFinder.SceneManager.LoadConnectionScenes(sceneLoadData);
