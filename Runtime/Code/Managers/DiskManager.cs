e1d94fab788a246efe5df24704598b1242fb111c
Luke Chatton <chattonluke@gmail.com>
2023-09-09 11:25:18 -0700

    return empty string

--- Runtime/Code/Managers/DiskManager.cs
+++ Runtime/Code/Managers/DiskManager.cs
@@ -4,27 +4,29 @@ using System.Threading.Tasks;
 using JetBrains.Annotations;
 using UnityEngine;
 
-[LuauAPI]
-public class DiskManager : Singleton<DiskManager> {
-    [CanBeNull]
-    public static async Task<string> ReadFileAsync(string path) {
-        var fullPath = Path.Join(Application.persistentDataPath, path);
-        if (!File.Exists(fullPath)) {
-            return null;
+namespace Code.Managers {
+    [LuauAPI]
+    public class DiskManager : Singleton<DiskManager> {
+        [CanBeNull]
+        public static async Task<string> ReadFileAsync(string path) {
+            var fullPath = Path.Join(Application.persistentDataPath, path);
+            if (!File.Exists(fullPath)) {
+                return "";
+            }
+
+            var contents = await File.ReadAllTextAsync(fullPath);
+            return contents;
         }
 
-        var contents = await File.ReadAllTextAsync(fullPath);
-        return contents;
-    }
-
-    public static async Task<bool> WriteFileAsync(string path, string contents) {
-        var fullPath = Path.Join(Application.persistentDataPath, path);
-        try {
-            await File.WriteAllTextAsync(fullPath, contents);
-            return true;
-        } catch (Exception e) {
-            Debug.LogError(e);
-            return false;
+        public static async Task<bool> WriteFileAsync(string path, string contents) {
+            var fullPath = Path.Join(Application.persistentDataPath, path);
+            try {
+                await File.WriteAllTextAsync(fullPath, contents);
+                return true;
+            } catch (Exception e) {
+                Debug.LogError(e);
+                return false;
+            }
         }
     }
 }
