9d7ceba75287ba4bce40f76fe0fefa1332a7cb02
Luke Chatton <chattonluke@gmail.com>
2023-11-09 13:42:18 -0800

    fix warnings

--- Runtime/ThirdParty/FishNet/CodeGenerating/Helpers/NetworkBehaviourHelper.cs
+++ Runtime/ThirdParty/FishNet/CodeGenerating/Helpers/NetworkBehaviourHelper.cs
@@ -402,7 +402,7 @@ namespace FishNet.CodeGenerating.Helping
             //If warning then also append warning text.
             if (loggingType != LoggingType.Off)
             {
-                string msg = $"Cannot complete action because client is not active. This may also occur if the object is not yet initialized, has deinitialized, or if it does not contain a NetworkObject component.";
+                string msg = $"Cannot complete action because client is not active. This may also occur if the object is not yet initialized, has deinitialized, or if it does not contain a NetworkObject component. [1] methodName={methodDef.Name}";
                 instructions.AddRange(base.GetClass<GeneralHelper>().LogMessage(methodDef, msg, loggingType));
             }
             //Add return.
@@ -453,7 +453,7 @@ namespace FishNet.CodeGenerating.Helping
             //If warning then also append warning text.
             if (loggingType != LoggingType.Off)
             {
-                string msg = $"Cannot complete action because server is not active. This may also occur if the object is not yet initialized, has deinitialized, or if it does not contain a NetworkObject component.";
+                string msg = $"Cannot complete action because server is not active. This may also occur if the object is not yet initialized, has deinitialized, or if it does not contain a NetworkObject component.  [2] methodName={methodDef.Name}";
                 instructions.AddRange(base.GetClass<GeneralHelper>().LogMessage(methodDef, msg, loggingType));
             }
             //Add return.
