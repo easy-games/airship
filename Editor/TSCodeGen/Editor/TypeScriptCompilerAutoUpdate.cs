9cabcda9534737e1ec64692ec0082dc832666afd
Jonathan Holmes <Vorlias@users.noreply.github.com>
2023-12-07 12:31:33 +1300

    Adjustments

--- Editor/TSCodeGen/Editor/TypeScriptCompilerAutoUpdate.cs
+++ Editor/TSCodeGen/Editor/TypeScriptCompilerAutoUpdate.cs
@@ -19,22 +19,24 @@ namespace Airship.Editor {
             var typeScriptDirectories = TypeScriptDirFinder.FindTypeScriptDirectories();
             
             // Get the remote version of unity-ts
-            var remoteVersion = NodePackages.GetCommandOutput(typeScriptDirectories[0], "view @easy-games/unity-ts version")[0];
-            
-            Debug.Log($"Latest Compiler Version is {remoteVersion}");
+            var remoteVersion = GetBuildVersion(NodePackages.GetCommandOutput(typeScriptDirectories[0], "view @easy-games/unity-ts version")[0]);
             
             foreach (var dir in typeScriptDirectories) {
                 var dirPkgInfo = NodePackages.ReadPackageJson(dir);
                 
                 var toolPackageJson = NodePackages.GetPackageInfo(dir, "@easy-games/unity-ts");
-                var toolVersion = toolPackageJson.Version;
+                var toolVersion = GetBuildVersion(toolPackageJson.Version);
 
-                if (toolVersion != remoteVersion) {
-                    Debug.LogWarning(
+                if (toolVersion < remoteVersion) {
+                    Debug.Log(
                         $"TS Project '{dirPkgInfo.Name}' - {toolVersion} != (remote) {remoteVersion} - updating to latest version");
                     NodePackages.RunNpmCommand(dir, $"install @easy-games/unity-ts@{remoteVersion}");
                 }
             }
+        }
+
+        private static int GetBuildVersion(string str) {
+            return int.Parse(str.Split("-")[1]);
         }
     }
 }
