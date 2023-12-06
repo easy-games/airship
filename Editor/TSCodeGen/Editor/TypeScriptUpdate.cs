0fed3a26d33f61c3248165acd85d3af0be1851f9
Jonathan Holmes <Vorlias@users.noreply.github.com>
2023-12-06 16:13:34 +1300

    Function to get each typescript package

--- Editor/TSCodeGen/Editor/TypeScriptUpdate.cs
+++ Editor/TSCodeGen/Editor/TypeScriptUpdate.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using CsToTs.TypeScript;
 using UnityEditor;
@@ -9,33 +10,49 @@ namespace Airship.Editor {
     [InitializeOnLoad]
     public class TypeScriptUpdate {
         private static bool _outdated = false;
+        private static string _remoteVersion;
         
         static TypeScriptUpdate() {
-            var typeDir = TypeScriptDirFinder.FindTypeScriptDirectory();
+            // var typeDir = TypeScriptDirFinder.FindTypeScriptDirectory();
+            var typeScriptDirectories = TypeScriptDirFinder.FindTypeScriptDirectories();
             
+            Debug.Log($"Found TypeScript Directories: {string.Join(", ", typeScriptDirectories)}");
+
             // Get the remote version of unity-ts
-            var version = NodePackages.GetCommandOutput(typeDir, "view @easy-games/unity-ts version")[0];
-
-            // Get the locally installed version of unity-ts
-            var toolPackageJson = NodePackages.GetPackageInfo(typeDir, "@easy-games/unity-ts");
-            var toolVersion = toolPackageJson.Version;
+            var remoteVersion = NodePackages.GetCommandOutput(typeScriptDirectories[0], "view @easy-games/unity-ts version")[0];
+            _remoteVersion = remoteVersion;
             
-            // Get the package.json version
-            var packageJson = NodePackages.ReadPackageJson(typeDir);
-            var packageVersion = packageJson.DevDependencies["@easy-games/unity-ts"].Substring(1);
+            Debug.Log($"Latest Compiler Version is {remoteVersion}");
             
+            foreach (var dir in typeScriptDirectories) {
+                var toolPackageJson = NodePackages.GetPackageInfo(dir, "@easy-games/unity-ts");
+                var toolVersion = toolPackageJson.Version;
 
-            if (toolVersion != packageVersion) {
-                Debug.LogWarning($"Locally installed version of TypeScript compiler mismatch: (package.json) != {toolVersion} (bin)");
-                _outdated = true;
+                if (toolVersion != remoteVersion) {
+                    Debug.LogWarning($"({dir}) {toolVersion} != (remote) {remoteVersion} - Will need update");
+                    _outdated = true;
+                }
             }
+            
+            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+        }
+        
+        private static void OnToolbarGUI()
+        {
+            if (Application.isPlaying) return;
 
-            if (toolVersion != version) {
-                Debug.LogWarning($"Latest TypeScript compiler version is {version} - you have {toolVersion} installed."); // lol
+            if (_outdated) {
+                GUILayout.FlexibleSpace();
                 
-                NodePackages.LoadAuthToken();
-                NodePackages.RunNpmCommand(typeDir, $"install @easy-games/unity-ts@{version}");
+                if (GUILayout.Button(new GUIContent("Update TS Compiler", "Your TypeScript compiler is out of date. Clicking this will update the version to the remote version"), ToolbarStyles.ServerLabelStyle)) {
+                    var typeDir = TypeScriptDirFinder.FindTypeScriptDirectory();
+                    NodePackages.LoadAuthToken();
+                    NodePackages.RunNpmCommand(typeDir, $"install @easy-games/unity-ts@{_remoteVersion}");
+                    _outdated = false;
+                }
             }
+
+            
         }
     }
 }
--- Runtime/Code/TSCodeGen/CsToTs/TypeScript/TypeScriptDirFinder.cs
+++ Runtime/Code/TSCodeGen/CsToTs/TypeScript/TypeScriptDirFinder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace CsToTs.TypeScript
@@ -28,6 +29,33 @@ namespace CsToTs.TypeScript
             }
 
             return null;
+        }
+        
+        public static string[] FindTypeScriptDirectories() {
+            List<string> dirs = new();
+            
+            var queue = new Queue<string>();
+            queue.Enqueue("Assets");
+
+            while (queue.Count > 0)
+            {
+                var dir = queue.Dequeue();
+                var packageJsonPath = Path.Join(dir, "package.json");
+
+                if (File.Exists(packageJsonPath))
+                {
+                    dirs.Add(dir);
+                    continue;
+                }
+
+                var subDirs = Directory.GetDirectories(dir);
+                foreach (var subDir in subDirs)
+                {
+                    queue.Enqueue(subDir);
+                }
+            }
+
+            return dirs.ToArray();
         }
     }
 }
