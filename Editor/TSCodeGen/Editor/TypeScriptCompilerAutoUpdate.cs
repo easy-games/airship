c4c66f7020b3c84a79a173727bbaf46538f1dd6a
Jonathan Holmes <Vorlias@users.noreply.github.com>
2023-12-08 13:16:06 +1300

    Include auto-update compiler types

--- Editor/TSCodeGen/Editor/TypeScriptCompilerAutoUpdate.cs
+++ Editor/TSCodeGen/Editor/TypeScriptCompilerAutoUpdate.cs
@@ -19,25 +19,29 @@ namespace Airship.Editor {
             NodePackages.LoadAuthToken();
             
             var typeScriptDirectories = TypeScriptDirFinder.FindTypeScriptDirectories();
-            
+            CheckUpdateForPackage(typeScriptDirectories, "@easy-games/unity-ts");
+            CheckUpdateForPackage(typeScriptDirectories, "@easy-games/compiler-types");
+        }
+
+        private static void CheckUpdateForPackage(string[] typeScriptDirectories,  string package) {
             // Get the remote version of unity-ts
-            var remoteVersion = NodePackages.GetCommandOutput(typeScriptDirectories[0], "view @easy-games/unity-ts version")[0];
+            var remoteVersion = NodePackages.GetCommandOutput(typeScriptDirectories[0], $"view {package} version")[0];
             var remoteVersionInt = GetBuildVersion(remoteVersion);
             
             foreach (var dir in typeScriptDirectories) {
                 var dirPkgInfo = NodePackages.ReadPackageJson(dir);
                 
-                var toolPackageJson = NodePackages.GetPackageInfo(dir, "@easy-games/unity-ts");
+                var toolPackageJson = NodePackages.GetPackageInfo(dir, package);
                 var toolVersion = GetBuildVersion(toolPackageJson.Version);
 
                 if (toolVersion < remoteVersionInt) {
                     Debug.Log(
                         $"TypeScript compiler for '{dirPkgInfo.Name}' is v{toolVersion}, latest is {remoteVersionInt} - updating to latest!");
-                    NodePackages.RunNpmCommand(dir, $"install @easy-games/unity-ts@{remoteVersion}");
+                    NodePackages.RunNpmCommand(dir, $"install {package}@{remoteVersion}");
                 }
             }
         }
-
+        
         private static int GetBuildVersion(string buildVersionString) {
             return int.Parse(buildVersionString.Split("-")[1]);
         }
