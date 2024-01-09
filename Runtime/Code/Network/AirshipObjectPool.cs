2f525e6a8583c1f43202c0f71f3d71c9ada184c7
Luke Chatton <chattonluke@gmail.com>
2024-01-08 22:10:21 -0800

    parent pooled entities to the object pool

--- Runtime/Code/Network/AirshipObjectPool.cs
+++ Runtime/Code/Network/AirshipObjectPool.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 namespace Code.Network {
     public class AirshipObjectPool : DefaultObjectPool {
         public int maxSpawnPerFrame = 5;
+
         public void SlowlyCacheObjects(NetworkObject prefab, int count) {
             if (count <= 0)
                 return;
@@ -22,11 +23,17 @@ namespace Code.Network {
             StartCoroutine(StartSlowSpawn(prefab, count));
         }
 
+        public override NetworkObject RetrieveObject(int prefabId, ushort collectionId, Vector3 position, Quaternion rotation, bool asServer) {
+            var obj = base.RetrieveObject(prefabId, collectionId, position, rotation, asServer);
+            obj.transform.SetParent(null);
+            return obj;
+        }
+
         private IEnumerator StartSlowSpawn(NetworkObject prefab, int count) {
             Stack<NetworkObject> cache = base.GetOrCreateCache(prefab.SpawnableCollectionId, prefab.PrefabId);
             for (int i = 0; i < count; i++)
             {
-                NetworkObject nob = Instantiate(prefab);
+                NetworkObject nob = Instantiate(prefab, this.transform);
                 nob.gameObject.SetActive(false);
                 cache.Push(nob);
                 if (i % this.maxSpawnPerFrame == 0) {
