edc582007a2c9d1a271cdc3ac741a41673b1e9d3
Luke Chatton <chattonluke@gmail.com>
2024-01-17 17:59:07 -0800

    add "latejoin:<seconds>" tag

--- Runtime/Code/Bootstrap/ClientNetworkConnector.cs
+++ Runtime/Code/Bootstrap/ClientNetworkConnector.cs
@@ -5,6 +5,7 @@ using FishNet.Transporting;
 
 #if UNITY_EDITOR
 using ParrelSync;
+using Unity.Multiplayer.Playmode;
 #endif 
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -28,9 +29,30 @@ public class ClientNetworkConnector : MonoBehaviour {
             if (!RunCore.IsEditor()) {
                 Debug.Log($"Connecting to server {transferData.address}:{transferData.port}");
             }
+
+#if UNITY_EDITOR
+            var tags = CurrentPlayer.ReadOnlyTags();
+            foreach (var tag in tags) {
+                if (tag.ToLower().StartsWith("latejoin:")) {
+                    var split = tag.ToLower().Split("latejoin:");
+                    if (split.Length == 2) {
+                        var num = int.Parse(split[1]);
+                        Debug.Log($"[Airship]: Delaying join by {num} seconds. This is due to having the {tag} MPPM tag.");
+                        StartCoroutine(LateJoin(num));
+                        return;
+                    }
+                }
+            }
+#endif
+
             // InstanceFinder.NetworkManager.ClientManager.StartConnection(transferData.address, transferData.port);
             InstanceFinder.NetworkManager.ClientManager.StartConnection();
         }
+    }
+
+    private IEnumerator LateJoin(int delaySeconds) {
+        yield return new WaitForSeconds(delaySeconds);
+        InstanceFinder.NetworkManager.ClientManager.StartConnection();
     }
     
     private void OnDisable()
