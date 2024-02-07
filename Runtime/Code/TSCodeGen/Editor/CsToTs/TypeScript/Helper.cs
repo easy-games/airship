8dc569e2fe851eb9135f45d33257e11bd2e29512
Luke Chatton <chattonluke@gmail.com>
2024-02-06 18:51:18 -0800

    dont use VisualScripting

--- Runtime/Code/TSCodeGen/Editor/CsToTs/TypeScript/Helper.cs
+++ Runtime/Code/TSCodeGen/Editor/CsToTs/TypeScript/Helper.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using HandlebarsDotNet;
-using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
