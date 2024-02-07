4a5e4528dc5133f56ee8e998df942c100a3a9a77
Luke Chatton <chattonluke@gmail.com>
2024-02-07 14:23:04 -0800

    migrate to publish v2 endpoints

---
+++ Editor/Publish/API/CreateDeploymentDto.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class CreateDeploymentDto {
+    public string gameId;
+    public string minPlayerVersion;
+    public string defaultScene;
+}
---
+++ Editor/Publish/API/DeploymentDto.cs
@@ -0,0 +1,41 @@
+using System;
+// ReSharper disable InconsistentNaming
+
+[Serializable]
+public class DeploymentDto {
+    public DeploymentUrls urls;
+    public DeploymentVersionDto version;
+}
+
+[Serializable]
+public class DeploymentVersionDto {
+    public string gameVersionId;
+    public string gameId;
+}
+
+[Serializable]
+public class DeploymentUrls {
+    public string Linux_client_resources;
+    public string Linux_shared_resources;
+    public string Linux_server_resources;
+    public string Linux_client_scenes;
+    public string Linux_shared_scenes;
+    public string Linux_server_scenes;
+
+    public string Mac_client_resources;
+    public string Mac_shared_resources;
+    // public string Mac_server_resources;
+    public string Mac_client_scenes;
+    public string Mac_shared_scenes;
+    // public string Mac_server_scenes;
+
+    public string Windows_client_resources;
+    public string Windows_shared_resources;
+    // public string Windows_server_resources;
+    public string Windows_client_scenes;
+    public string Windows_shared_scenes;
+    // public string Windows_server_scenes;
+
+    public string gameConfig;
+    public string source;
+}
