df51717081c93f0e558829076e62e8571e0d91a5
Tshadow0508 <tshadow05@gmail.com>
2024-02-26 10:15:49 -0800

    Face accessory

---
+++ Runtime/Code/Player/Accessories/AccesoryFace.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FaceAccessory", menuName = "Airship/Accessories/Face Accessory")]
+public class AccesoryFace : ScriptableObject
+{
+    public string serverClassId;
+    public Texture2D decalTexture;
+
+}
