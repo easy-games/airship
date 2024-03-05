d3e3c18c6de97eb0fecb95e32095364d1d107e26
Rob-bie <deibbor@gmail.com>
2024-03-05 13:41:45 -0600

    Add new packages to root `.gitignore`.

--- Editor/Packages/AirshipPackagesWindow.cs
+++ Editor/Packages/AirshipPackagesWindow.cs
@@ -173,11 +173,6 @@ namespace Editor.Packages {
                 EditorGUILayout.Space(12);
                 EditorGUILayout.BeginVertical();
                 this.addPackageId = EditorGUILayout.TextField("Package ID", this.addPackageId);
-                // this.addVersionToggle = EditorGUILayout.BeginToggleGroup("Version", this.addVersionToggle);
-                // if (this.addVersionToggle) {
-                //     this.addPackageVersion = EditorGUILayout.TextField("Version", this.addPackageVersion);
-                // }
-                // EditorGUILayout.EndToggleGroup();
                 EditorGUILayout.Space(4);
                 if (GUILayout.Button("Add Package", GUILayout.Width(150))) {
                     if (this.addVersionToggle) {
@@ -655,6 +650,22 @@ namespace Editor.Packages {
                         }
                     }
                 }
+            }
+            
+            // Add package to .gitignore
+            
+            var rootGitIgnore = $"{Path.GetDirectoryName(Application.dataPath)}/.gitignore";
+            var lines = File.ReadLines(rootGitIgnore);
+            
+            var srcIgnore = $"Assets/Bundles/{packageId}/*";
+            var metaIgnore = $"Assets/Bundles/{packageId}.meta";
+
+            if (!lines.Contains(srcIgnore)) {
+                File.AppendAllLines(rootGitIgnore, new List<string>(){ $"\n{srcIgnore}" });
+            }
+
+            if (!lines.Contains(metaIgnore)) {
+                File.AppendAllLines(rootGitIgnore, new List<string>(){ $"\n{metaIgnore}" });
             }
 
 
