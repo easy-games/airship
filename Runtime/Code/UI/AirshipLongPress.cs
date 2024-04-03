97992ae3d2e90f068feac8c2a8b086abc9161f9b
Luke Chatton <chattonluke@gmail.com>
2024-04-03 12:43:13 -0700

    Update AirshipLongPress.cs

--- Runtime/Code/UI/AirshipLongPress.cs
+++ Runtime/Code/UI/AirshipLongPress.cs
@@ -5,20 +5,23 @@ using UnityEngine.EventSystems;
 using UnityEngine.Events;
 using System;
 
-public class AirshipLongPress : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler {
+public class AirshipLongPress : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IDragHandler, IBeginDragHandler {
     [Tooltip("Time before the long press triggered")]
-    public float holdTime = 0.2f;
+    public float holdTime = 0.22f;
 
     private float pressStartTime = 0f;
     private bool pressing = false;
+    private Vector2 pressPosition;
+    private IDragHandler dragHandlerImplementation;
 
     public event Action OnClick;
-    public event Action OnLongPress;
+    public event Action<object> OnLongPress;
 
     public void OnPointerDown(PointerEventData eventData) {
         this.OnClick?.Invoke();
         this.pressStartTime = Time.time;
         this.pressing = true;
+        this.pressPosition = eventData.pressPosition;
     }
 
     public void OnPointerUp(PointerEventData eventData)
@@ -36,7 +39,15 @@ public class AirshipLongPress : MonoBehaviour, IPointerDownHandler, IPointerUpHa
     public void Update() {
         if (this.pressing && Time.time - this.pressStartTime >= this.holdTime) {
             this.pressing = false;
-            this.OnLongPress?.Invoke();
+            this.OnLongPress?.Invoke(this.pressPosition);
         }
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        this.pressing = false;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        this.pressing = false;
     }
 }
