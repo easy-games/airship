54bbdc7760916183abf6bf52f601a8838d6b359e
liamlutton <lutton+gh@me.com>
2024-04-04 16:48:34 -0700

    Process presets (hopefully)

---
+++ Runtime/Code/Airship/Resources/Scripts/Editor/AirshipAssetPresets.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.Presets;
+using UnityEngine;
+
+namespace Code.Airship.Resources.Scripts.Editor {
+    public class AirshipAssetPresets {
+        private static string presetsPath = "Packages/gg.easy.airship/Runtime/Presets";
+        public class EnforcePresetPostProcessor : AssetPostprocessor {
+            void OnPreprocessAsset() {
+                if (!AssetDatabase.IsValidFolder(assetPath) && !assetPath.EndsWith(".cs") &&
+                    !assetPath.EndsWith(".preset")) {
+                    if (!AssetDatabase.IsValidFolder(presetsPath)) return;
+
+                    var presetPaths = Directory.EnumerateFiles(presetsPath, "*.preset", SearchOption.AllDirectories);
+                    foreach (var path in presetPaths) {
+                        var preset = AssetDatabase.LoadAssetAtPath<Preset>(path);
+                        if (preset == null) continue;
+
+                        preset.ApplyTo(assetImporter);
+                    }
+                }
+            }
+        }
+    }
+}
