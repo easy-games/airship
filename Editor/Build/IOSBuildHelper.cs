a009ea713a5da9cd5d862cd00d11a34aaee92769
Luke Chatton <chattonluke@gmail.com>
2024-04-05 12:41:25 -0700

    add post processor to update info.plist

---
+++ Editor/Build/IOSBuildHelper.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+#endif
+using System.IO;
+using System.Collections.Generic;
+
+public class IOSBuildProcessor {
+#if UNITY_IOS
+    [PostProcessBuild]
+    static void OnPostprocessBuild(BuildTarget buildTarget, string path)
+    {
+        // Read plist
+        var plistPath = Path.Combine(path, "Info.plist");
+        var plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+
+        // Update value
+        PlistElementDict rootDict = plist.root;
+        rootDict.SetString("NSCameraUsageDescription", "Used for taking profile pictures.");
+
+        // Write plist
+        File.WriteAllText(plistPath, plist.WriteToString());
+    }
+#endif
+}
