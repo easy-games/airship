3f751167c2bec7501be5951cd705542e969ee12f
Rob-bie <deibbor@gmail.com>
2024-04-08 14:31:36 -0500

    Re-add jump multiplier support.

--- Runtime/Code/Player/Character/CharacterMovement.cs
+++ Runtime/Code/Player/Character/CharacterMovement.cs
@@ -647,6 +647,7 @@ namespace Code.Player.Character {
 				CharacterMoveModifier modifierFromEvent = new CharacterMoveModifier()
 				{
 					speedMultiplier = 1,
+					jumpMultiplier = 1,
 					blockSprint = false,
 					blockJump = false,
 				};
@@ -700,7 +701,7 @@ namespace Code.Player.Character {
 				if (canJump) {
 					// Jump
 					didJump = true;
-					velocity.y = moveData.jumpSpeed;
+					velocity.y = moveData.jumpSpeed * characterMoveModifier.jumpMultiplier;
 					prevJumpStartPos = transform.position;
 
 					if (!replaying) {
