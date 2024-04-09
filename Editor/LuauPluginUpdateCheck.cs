8e74b770810c98806465315b2b76c4c5748e5ee9
Stephen Leitnick <sleitnick@gmail.com>
2024-04-09 16:41:06 -0400

    Check file update time before attempting hash comparison

--- Editor/LuauPluginUpdateCheck.cs
+++ Editor/LuauPluginUpdateCheck.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 
 public static class LuauPluginUpdateCheck {
     private const string BasePath = "Packages/gg.easy.airship/Runtime/Plugins";
-    private const string SessionKey = "LuauPluginHash";
+    private const string SessionKeyWriteTime = "LuauPluginWriteTime";
+    private const string SessionKeyHash = "LuauPluginHash";
     
 #if UNITY_EDITOR_WIN
     private const string PluginPath = BasePath + "/Windows/x64/LuauPlugin.dll";
@@ -17,7 +18,7 @@ public static class LuauPluginUpdateCheck {
 #if UNITY_EDITOR_WIN || UNITY_EDITOR_OSX
     [UnityEditor.Callbacks.DidReloadScripts]
     private static void CheckHash() {
-        if (!EditorIntegrationsConfig.instance.promptIfLuauPluginChanged) {
+        if (Application.isPlaying || !EditorIntegrationsConfig.instance.promptIfLuauPluginChanged) {
             return;
         }
         
@@ -25,20 +26,26 @@ public static class LuauPluginUpdateCheck {
             Debug.LogWarning($"Could not compare Luau plugin hash; plugin at path not found: {PluginPath}");
             return;
         }
+
+        // Check last write time:
+        var pluginLastWriteTime = File.GetLastWriteTime(PluginPath).ToString("O");
+        var lastWriteTime = SessionState.GetString(SessionKeyWriteTime, "");
+        if (pluginLastWriteTime == lastWriteTime) return;
         
-        var pluginBytes = File.ReadAllBytes(PluginPath);
+        SessionState.SetString(SessionKeyWriteTime, pluginLastWriteTime);
         
         // Create MD5 hash of the plugin:
+        var pluginBytes = File.ReadAllBytes(PluginPath);
         using var md5 = MD5.Create();
         md5.Initialize();
         md5.ComputeHash(pluginBytes);
         var hash = Convert.ToBase64String(md5.Hash);
 
         // Get the last hash saved in session state, if any:
-        var lastHash = SessionState.GetString(SessionKey, "");
+        var lastHash = SessionState.GetString(SessionKeyHash, "");
         if (hash == lastHash) return;
         
-        SessionState.SetString(SessionKey, hash);
+        SessionState.SetString(SessionKeyHash, hash);
         
         // Hash didn't match and last hash in session state isn't empty, so the plugin file changed: 
         if (lastHash != "") {
