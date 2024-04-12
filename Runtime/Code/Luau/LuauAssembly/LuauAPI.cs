dddfb7eaadc2f8e321e14d8e818f5347b19fc1ae
Stephen Leitnick <sleitnick@gmail.com>
2024-04-12 14:56:12 -0400

    Luau attribute allows context whitelist

--- Runtime/Code/Luau/LuauAssembly/LuauAPI.cs
+++ Runtime/Code/Luau/LuauAssembly/LuauAPI.cs
@@ -1,2 +1,23 @@
+using System;
+
 public class LuauAPI : System.Attribute {
+    public readonly LuauContext AllowedContextsMask;
+    
+    public LuauAPI(params LuauContext[] allowedContexts) {
+        LuauContext mask = 0;
+        
+        if (allowedContexts.Length > 0) {
+            // Only allow the contexts provided
+            foreach (var context in allowedContexts) {
+                mask |= context;
+            }
+        } else {
+            // Default behavior - allow all contexts if no contexts were provided
+            foreach (LuauContext context in Enum.GetValues(typeof(LuauContext))) {
+                mask |= context;
+            }
+        }
+        
+        AllowedContextsMask = mask;
+    }
 }
---
+++ Runtime/Code/Luau/LuauAssembly/LuauContext.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// Identifiers for the various Luau states. Should match up with the Luau plugin "LuauContext.h" enum
+/// </summary>
+[System.Flags]
+public enum LuauContext {
+    /// <summary>
+    /// Game context exist per-game. It is spun up and tore down between games.
+    /// </summary>
+    Game = 1 << 0,
+    
+    /// <summary>
+    /// Protected context always exists and runs the duration of the main application.
+    /// </summary>
+    Protected = 1 << 1,
+}
