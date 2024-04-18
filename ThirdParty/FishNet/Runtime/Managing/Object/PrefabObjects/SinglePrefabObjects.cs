af01859568cc9faa47abe19dab6895bca37ebfb0
Luke Chatton <chattonluke@gmail.com>
2024-04-18 15:52:12 -0700

    add notice about NetworkPrefabCollection

--- ThirdParty/FishNet/Runtime/Managing/Object/PrefabObjects/SinglePrefabObjects.cs
+++ ThirdParty/FishNet/Runtime/Managing/Object/PrefabObjects/SinglePrefabObjects.cs
@@ -35,7 +35,7 @@ namespace FishNet.Managing.Object
         {
             if (id < 0 || id >= _prefabs.Count)
             {
-                NetworkManagerExtensions.LogError($"PrefabId {id} is out of range.");
+                NetworkManagerExtensions.LogError($"PrefabId {id} is out of range. Make sure all network prefabs are in Assets/Bundles/Shared/Resources/NetworkPrefabCollection.asset");
                 return null;
             }
             else
