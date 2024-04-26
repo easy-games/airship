bd0b20c1ec0f005d3d7fca91d007c866950d793c
Rob-bie <deibbor@gmail.com>
2024-04-25 21:22:42 -0500

    Digging for treasure.

--- Runtime/Code/Network/AirshipObjectPool.cs
+++ Runtime/Code/Network/AirshipObjectPool.cs
@@ -25,6 +25,7 @@ namespace Code.Network {
         }
 
         public override NetworkObject RetrieveObject(int prefabId, ushort collectionId, Transform parent = null, Vector3? nullableLocalPosition = null, Quaternion? nullableLocalRotation = null, Vector3? nullableLocalScale = null, bool makeActive = true, bool asServer = true) {
+            Debug.Log($"[FishNet] AirshipObjectPool RetrieveObject | Prefab Id: {prefabId} Collection Id: {collectionId}");
             var obj = base.RetrieveObject(prefabId, collectionId, parent, nullableLocalPosition, nullableLocalRotation, nullableLocalScale, makeActive, asServer);
             obj.transform.SetParent(null);
             return obj;
