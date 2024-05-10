394b08796a221bcaa98c0c00dec471dae78c728f
Luke Chatton <chattonluke@gmail.com>
2024-05-10 10:36:08 -0700

    ignore empty airshipGUID (fixes old games)

--- ThirdParty/FishNet/Runtime/Managing/Server/Object/ServerObjects.cs
+++ ThirdParty/FishNet/Runtime/Managing/Server/Object/ServerObjects.cs
@@ -445,31 +445,35 @@ namespace FishNet.Managing.Server
             bool predictedSpawn = false;
 
             // Airship: fix for scenes referencing network prefabs
-            bool replaced = false;
-            if (NetworkManager.SpawnablePrefabs.GetObjectCount() > 0) {
-                for (int i = 0; i < NetworkManager.SpawnablePrefabs.GetObjectCount(); i++) {
-                    var savedNob = NetworkManager.SpawnablePrefabs.GetObject(true, i);
-                    if (savedNob.airshipGUID == networkObject.airshipGUID) {
-                        networkObject.PrefabId = savedNob.PrefabId;
-                        networkObject.SpawnableCollectionId = savedNob.SpawnableCollectionId;
-                        replaced = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!replaced && NetworkManager.RuntimeSpawnablePrefabs.Count > 0) {
-                // intentionally starting at 1
-                for (ushort x = 1; x < NetworkManager.RuntimeSpawnablePrefabs.Count; x++) {
-                    var prefabs = NetworkManager.RuntimeSpawnablePrefabs[x];
-                    if (prefabs.GetObjectCount() == 0) continue;
-                    for (int i = 0; i < prefabs.GetObjectCount(); i++) {
-                        var savedNob = prefabs.GetObject(true, i);
+            if (!string.IsNullOrEmpty(networkObject.airshipGUID)) {
+                bool replaced = false;
+                if (NetworkManager.SpawnablePrefabs.GetObjectCount() > 0) {
+                    for (int i = 0; i < NetworkManager.SpawnablePrefabs.GetObjectCount(); i++) {
+                        var savedNob = NetworkManager.SpawnablePrefabs.GetObject(true, i);
+                        if (string.IsNullOrEmpty(savedNob.airshipGUID)) continue;
                         if (savedNob.airshipGUID == networkObject.airshipGUID) {
                             networkObject.PrefabId = savedNob.PrefabId;
                             networkObject.SpawnableCollectionId = savedNob.SpawnableCollectionId;
                             replaced = true;
                             break;
+                        }
+                    }
+                }
+
+                if (!replaced && NetworkManager.RuntimeSpawnablePrefabs.Count > 0) {
+                    // intentionally starting at 1
+                    for (ushort x = 1; x < NetworkManager.RuntimeSpawnablePrefabs.Count; x++) {
+                        var prefabs = NetworkManager.RuntimeSpawnablePrefabs[x];
+                        if (prefabs.GetObjectCount() == 0) continue;
+                        for (int i = 0; i < prefabs.GetObjectCount(); i++) {
+                            var savedNob = prefabs.GetObject(true, i);
+                            if (string.IsNullOrEmpty(savedNob.airshipGUID)) continue;
+                            if (savedNob.airshipGUID == networkObject.airshipGUID) {
+                                networkObject.PrefabId = savedNob.PrefabId;
+                                networkObject.SpawnableCollectionId = savedNob.SpawnableCollectionId;
+                                replaced = true;
+                                break;
+                            }
                         }
                     }
                 }
