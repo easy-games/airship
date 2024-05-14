2c2c26ef4e355365c8237571459058ef51d0d3a6
Tshadow0508 <tshadow05@gmail.com>
2024-05-14 12:46:21 -0700

    Moved setupManager to an editor folder so its not compiled in builds

--- Runtime/Code/ProjectSetup/Editor/SetupManager.cs
+++ Runtime/Code/ProjectSetup/Editor/SetupManager.cs
@@ -1,33 +1,24 @@
 using UnityEngine;
-#if UNITY_EDITOR
 using UnityEditor;
 
 [InitializeOnLoad]
-#endif
 public class SetupManager : AssetPostprocessor{
 
     private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths){
-#if UNITY_EDITOR
         if (!SessionState.GetBool("FirstAirshipSettingsInitDone", false)) {
             // Startup code here
             FixProject();
          
             SessionState.SetBool("FirstAirshipSettingsInitDone", true);
         }
-#endif
     }
     
-
-#if UNITY_EDITOR
     [MenuItem("Airship/Misc/Repair Project")]
-    #endif
     public static void FixProject()
     {
-#if UNITY_EDITOR
         Debug.Log("Setting up Airship Project Settings");
         FishNetSetup.Setup();
         var config = MiscProjectSetup.Setup();
         PhysicsSetup.Setup(config);
-#endif
     }
 }
