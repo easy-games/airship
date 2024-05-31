a3d07bbbb4c1b92474dc81c9891da926862706da
Luke Chatton <chattonluke@gmail.com>
2024-05-31 12:44:05 -0700

    fix short paths from game

--- Runtime/Code/Bundles/AssetBridge.cs
+++ Runtime/Code/Bundles/AssetBridge.cs
@@ -105,20 +105,19 @@ public class AssetBridge : IAssetBridge
 		if (path.StartsWith("assets/")) {
 			path = path.Substring(7);
 		}
-		var split = path.Split("/");
-
-		if (split.Length < 3) {
-			if (printErrorOnFail)
-			{
-				Debug.LogError($"Failed to load invalid asset path: \"{path}\"");
-			}
-			return null;
-		}
 
 		string importedPackageName; // ex: "@Easy/Core" or "" for game package.
 		bool isImportedPackage;
 		string assetBundleFile;
 		if (path.Contains("@")) {
+			var split = path.Split("/");
+			if (split.Length < 2) {
+				if (printErrorOnFail) {
+					Debug.LogError($"Failed to load invalid asset path: \"{path}\"");
+				}
+				return null;
+			}
+
 			importedPackageName = split[1] + "/" + split[2];
 			isImportedPackage = true;
 			assetBundleFile = "shared/resources";
