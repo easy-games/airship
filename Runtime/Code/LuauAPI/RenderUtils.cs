513c4271e9c412c3af815014d84bfcbb4a6cdb3e
Tshadow0508 <tshadow05@gmail.com>
2024-06-06 13:28:25 -0700

    formatting change

--- Runtime/Code/LuauAPI/RenderUtils.cs
+++ Runtime/Code/LuauAPI/RenderUtils.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 [LuauAPI]
 public static class RenderUtils{
     public static RenderTexture CreateDefaultRenderTexture(int width, int height){
-        RenderTextureDescriptor descriptor = new RenderTextureDescriptor(width, height, RenderTextureFormat.ARGB32, 8,0);
-        descriptor.sRGB = false;
-        descriptor.autoGenerateMips = false;
-        descriptor.useMipMap = false;
+        RenderTextureDescriptor descriptor = new RenderTextureDescriptor(width, height, RenderTextureFormat.ARGB32, 8, 0) {
+            sRGB = false,
+            autoGenerateMips = false,
+            useMipMap = false
+        };
         return new RenderTexture(descriptor);
     }
 }
