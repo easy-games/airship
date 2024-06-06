83692461bb34d4491cd1589a5b9df4e203392205
Tshadow0508 <tshadow05@gmail.com>
2024-06-06 13:27:29 -0700

    got character to render in avatarEditor

--- Runtime/Code/LuauAPI/RenderUtils.cs
+++ Runtime/Code/LuauAPI/RenderUtils.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [LuauAPI]
 public static class RenderUtils{
     public static RenderTexture CreateDefaultRenderTexture(int width, int height){
-        RenderTextureDescriptor descriptor = new RenderTextureDescriptor(width, height, RenderTextureFormat.ARGB32, 0,0);
+        RenderTextureDescriptor descriptor = new RenderTextureDescriptor(width, height, RenderTextureFormat.ARGB32, 8,0);
         descriptor.sRGB = false;
         descriptor.autoGenerateMips = false;
         descriptor.useMipMap = false;
