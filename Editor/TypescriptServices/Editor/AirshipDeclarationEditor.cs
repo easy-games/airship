6064f2073691c9493db0b0609ae8e7eafb66fd57
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-06-22 00:02:45 +1200

    Declaration script source + better declaration icn

---
+++ Editor/TypescriptServices/Editor/AirshipDeclarationEditor.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Luau;
+using UnityEditor;
+using UnityEngine;
+
+namespace Airship.Editor {
+    [CustomEditor(typeof(DeclarationFile))]
+    public class AirshipDeclarationEditor : AirshipScriptEditor<DeclarationFile> {
+        private const string IconDeclaration = "Packages/gg.easy.airship/Editor/TypescriptDeclaration.png";
+        private string assetPath;
+        private BinaryFile asset;
+        
+       protected override void OnHeaderGUI() {
+            GUILayout.Space(10f);
+            var rect = EditorGUILayout.GetControlRect(false, 40, "IN BigTitle");
+        
+            var textureImage = new Rect(rect);
+            textureImage.y += 0;
+            textureImage.x += 0;
+            textureImage.width = 38;
+            textureImage.height = 38;
+
+            var icon = "";
+            rect.x += 40;
+            
+            if (item != null) {
+                GUI.Label(rect, ObjectNames.NicifyVariableName(Path.GetFileNameWithoutExtension(item.name)), "IN TitleText");
+
+                var label = "Typescript Declaration";
+                if (asset != null) {
+                    label = "Typescript Luau Declaration";
+                } else if (item.ambient) {
+                    label = "Typescript Ambient Declaration";
+                }
+                
+                GUI.Label(new RectOffset(2, 0, -10, 0).Add(rect), label);
+                icon = IconDeclaration;
+            }
+            else {
+                GUI.Label(rect, "Multiple Scripts", "IN TitleText");
+                GUI.Label(new RectOffset(2, 0, -10, 0).Add(rect), "Multiple Scripts Selected");
+                icon = IconDeclaration;
+            }
+            
+            var flag = AssetPreview.IsLoadingAssetPreview(this.target.GetInstanceID());
+            var image = AssetDatabase.LoadAssetAtPath<Texture2D>(icon);
+            
+            
+            if (!(bool) (UnityEngine.Object) image)
+            {
+                if (flag)
+                    Repaint();
+                image = AssetPreview.GetMiniThumbnail(this.target);
+            }
+            
+            GUI.Label(textureImage, image);
+            
+            EditorGUILayout.BeginHorizontal();
+            {
+                if (items != null) {
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("Reimport All", GUILayout.MaxWidth(100))) {
+                        AssetDatabase.StartAssetEditing();
+                        foreach (var script in items) {
+                            AssetDatabase.ImportAsset(script.scriptPath, ImportAssetOptions.Default);
+                        }
+                        AssetDatabase.StopAssetEditing();
+                        
+                        UpdateSelection();
+                        return;
+                    }
+                } else if (item != null) {
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("Reimport", GUILayout.MaxWidth(100))) {
+           
+                        AssetDatabase.StartAssetEditing();
+                        AssetDatabase.ImportAsset(assetPath, ImportAssetOptions.Default);
+                        AssetDatabase.StopAssetEditing();
+                        
+                        UpdateSelection();
+                        return;
+                    }
+                    
+                    if (GUILayout.Button("Edit", GUILayout.MaxWidth(100))) {
+                        TypescriptProjectsService.OpenFileInEditor(assetPath);
+                    }
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+            
+            GUILayout.Space(10f);
+        }
+        
+        public override void OnInspectorGUI() {
+            if (!item) return;
+            GUI.enabled = true;
+            
+            GUILayout.Label("Declaration File", EditorStyles.boldLabel);
+                
+            GUI.enabled = false;
+            EditorGUILayout.ObjectField("Luau File", asset, typeof(BinaryFile), false);
+
+            GUI.enabled = true;
+            DrawSourceText();
+            GUI.enabled = false;
+        }
+        
+        private void UpdateSelection() {
+            if (targets.Length > 1) {
+                // items = targets.Select(target => target as BinaryFile);
+            }
+            else {
+                item = target as DeclarationFile;
+                assetPath = AssetDatabase.GetAssetPath(item);
+                
+                var scriptPath = item.scriptPath;
+                if (scriptPath != "") {
+                    asset = AssetDatabase.LoadAssetAtPath<BinaryFile>(scriptPath);
+                }
+                else {
+                    asset = null;
+                }
+            
+                CachePreview();        
+            }
+            
+            CachePreview();
+        }
+
+        protected override void OnEnable() {
+            base.OnEnable();
+            UpdateSelection();
+        }
+
+        protected override void OnDisable() {
+            base.OnDisable();
+            asset = null;
+        }
+    }
+}
---
+++ Editor/TypescriptServices/Editor/AirshipScriptFileEditor.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Luau;
+using UnityEditor;
+using UnityEngine;
+
+namespace Airship.Editor {
+    public static class AirshipScriptContextMenus {
+        [MenuItem("CONTEXT/ScriptBinding/Reset", priority = 0)]
+        public static void Test(MenuCommand command) {
+            var binding = command.context as ScriptBinding;
+            if (binding == null || binding.m_metadata == null) return;
+            foreach (var property in binding.m_metadata.properties.Where(property => property.modified)) {
+                property.SetDefaultAsValue();
+                property.modified = false;
+            }
+                
+            EditorUtility.SetDirty(binding);
+        }
+        
+        
+        
+        [MenuItem("CONTEXT/ScriptBinding/Edit Script")]
+        public static void EditScript(MenuCommand command) {
+            var binding = command.context as ScriptBinding;
+            if (binding == null || binding.m_metadata == null) return;
+
+            TypescriptProjectsService.OpenFileInEditor(binding.scriptFile.assetPath);
+        }
+
+        [MenuItem("CONTEXT/ScriptBinding/Edit Script", validate = true)]
+        [MenuItem("CONTEXT/ScriptBinding/Remove Script", validate = true)]
+        public static bool ValidateRemoveScript(MenuCommand command) {
+            var binding = command.context as ScriptBinding;
+            return binding != null && binding.scriptFile != null;
+        }
+        
+        [MenuItem("CONTEXT/ScriptBinding/Remove Script")]
+        public static void RemoveScript(MenuCommand command) {
+            var binding = command.context as ScriptBinding;
+            if (binding == null || binding.m_metadata == null) return;
+
+            binding.scriptFile = null;
+            binding.m_fileFullPath = null;
+        }
+    }
+
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(BinaryFile))]
+    public class AirshipScriptFileEditor : AirshipScriptEditor<BinaryFile> {
+        private const string IconOk = "Packages/gg.easy.airship/Editor/TypescriptAsset.png";
+        private const string IconEmpty = "Packages/gg.easy.airship/Editor/TypescriptOff.png";
+        private const string IconFail = "Packages/gg.easy.airship/Editor/TypescriptErr.png";
+        
+        private const string LuaIconOk = "Packages/gg.easy.airship/Editor/LuauAssetIcon.png";
+        private const string LuaIconFail = "Packages/gg.easy.airship/Editor/LuauAssetIconError.png";
+        
+        // private BinaryFile script;
+        // private IEnumerable<BinaryFile> scripts;
+        
+        private DeclarationFile declaration;
+        private string assetGuid;
+        
+        private void UpdateSelection() {
+            if (targets.Length > 1) {
+                items = targets.Select(target => target as BinaryFile);
+            }
+            else {
+                item = target as BinaryFile;
+                var assetPath = AssetDatabase.GetAssetPath(item);
+                assetGuid = AssetDatabase.AssetPathToGUID(assetPath);
+                declaration = null;
+                if (item.scriptLanguage == AirshipScriptLanguage.Luau) {
+                    var declarationPath = assetPath.Replace(".lua", ".d.ts").Replace("init", "index");
+                    if (File.Exists(declarationPath)) {
+                        declaration = AssetDatabase.LoadAssetAtPath<DeclarationFile>(declarationPath);
+                    }
+                }
+            
+                CachePreview();        
+            }
+        }
+
+        protected override void OnEnable() {
+            UpdateSelection();
+            base.OnEnable();
+           
+        }
+
+        protected override void OnHeaderGUI() {
+            GUILayout.Space(10f);
+            var rect = EditorGUILayout.GetControlRect(false, 40, "IN BigTitle");
+        
+            var textureImage = new Rect(rect);
+            textureImage.y += 0;
+            textureImage.x += 0;
+            textureImage.width = 38;
+            textureImage.height = 38;
+
+            var icon = "";
+            rect.x += 40;
+            
+            if (item != null) {
+                switch (item.scriptLanguage) {
+                    case AirshipScriptLanguage.Luau: {
+                        rect.y += 6;
+                        GUI.Label(rect, ObjectNames.NicifyVariableName(item.name), "IN TitleText");
+                        icon = LuaIconOk;
+                        break;
+                    }
+                    case AirshipScriptLanguage.Typescript: {
+                        if (item.airshipBehaviour && item.m_metadata != null) {
+                            GUI.Label(rect, item.m_metadata.displayName, "IN TitleText");
+                            GUI.Label(new RectOffset(2, 0, -10, 0).Add(rect), "Airship Component");
+                        }
+                        else {
+                            rect.y += 6;
+                            GUI.Label(rect, ObjectNames.NicifyVariableName(item.name), "IN TitleText");
+                        }
+                    
+                        icon = IconOk;
+                        break;
+                    }
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            else {
+                GUI.Label(rect, "Multiple Scripts", "IN TitleText");
+                GUI.Label(new RectOffset(2, 0, -10, 0).Add(rect), "Multiple Scripts Selected");
+                icon = IconOk;
+            }
+            
+            var flag = AssetPreview.IsLoadingAssetPreview(this.target.GetInstanceID());
+            var image = item.m_metadata?.displayIcon != null ? item.m_metadata?.displayIcon : AssetDatabase.LoadAssetAtPath<Texture2D>(icon);
+            
+            
+            if (!(bool) (UnityEngine.Object) image)
+            {
+                if (flag)
+                    Repaint();
+                image = AssetPreview.GetMiniThumbnail(this.target);
+            }
+            
+            GUI.Label(textureImage, image);
+            
+            EditorGUILayout.BeginHorizontal();
+            {
+                if (items != null) {
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("Reimport All", GUILayout.MaxWidth(100))) {
+                        AssetDatabase.StartAssetEditing();
+                        foreach (var script in items) {
+                            AssetDatabase.ImportAsset(script.assetPath, ImportAssetOptions.Default);
+                        }
+                        AssetDatabase.StopAssetEditing();
+                        
+                        UpdateSelection();
+                        return;
+                    }
+                } else if (item != null) {
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("Reimport", GUILayout.MaxWidth(100))) {
+           
+                        AssetDatabase.StartAssetEditing();
+                        AssetDatabase.ImportAsset(item.assetPath, ImportAssetOptions.Default);
+                        AssetDatabase.StopAssetEditing();
+                        
+                        UpdateSelection();
+                        return;
+                    }
+                    
+                    if (GUILayout.Button("Edit", GUILayout.MaxWidth(100))) {
+                        TypescriptProjectsService.OpenFileInEditor(item.assetPath);
+                    }
+            
+                    if (item.scriptLanguage == AirshipScriptLanguage.Typescript) {
+                        GUI.enabled = item != null && item.m_compiled;
+                        if (GUILayout.Button("View Compiled", GUILayout.MaxWidth(150)))
+                        {
+                            var project = TypescriptProjectsService.Project;
+                            TypescriptProjectsService.OpenFileInEditor(project.GetOutputPath(item.assetPath));
+                        }                   
+                    }
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+            
+            GUILayout.Space(10f);
+        }
+
+        public (string typeName, bool isArray, bool isObject) GetType(LuauMetadataProperty property) {
+            var typeName = property.type;
+            var isArray = false;
+            var isObject = false;
+
+            if (property.type is "Array") {
+                isArray = true;
+
+                if (property.items.type is "object") {
+                    typeName = property.items.objectType;
+                }
+                else {
+                    typeName = property.items.type;
+                }
+            } else if (property.type is "AirshipBehaviour" or "object") {
+                typeName = property.objectType;
+                isObject = true;
+            } else if (property.type is "StringEnum" or "IntEnum") {
+                typeName = property.refPath.Split("@")[1];
+            }
+
+            return (typeName, isArray, isObject);
+        }
+
+        public override void OnInspectorGUI() {
+            GUI.enabled = true;
+
+            if (item != null) {
+                if (item.scriptLanguage == AirshipScriptLanguage.Typescript && item.airshipBehaviour) {
+                    EditorGUILayout.Space(10);
+                    GUILayout.Label("Component Details", EditorStyles.boldLabel);
+                    
+                    EditorGUILayout.LabelField("DisplayName", item.m_metadata!.displayName, EditorStyles.boldLabel);
+                    EditorGUILayout.LabelField("ClassName", item.m_metadata.name, scriptTextMono);
+
+                    EditorGUILayout.Space(10);
+                    GUILayout.Label("Properties", EditorStyles.boldLabel);
+                    foreach (var property in item.m_metadata.properties) {
+                        var typeInfo = GetType(property);
+                        
+                        if (typeInfo.isArray) {
+                            EditorGUILayout.LabelField(ObjectNames.NicifyVariableName(property.name), $"{typeInfo.typeName}[]", scriptTextMono);
+                        }
+                        else {
+                            EditorGUILayout.LabelField(ObjectNames.NicifyVariableName(property.name), typeInfo.typeName, scriptTextMono);
+                        }
+                    }
+                } else if (item.scriptLanguage == AirshipScriptLanguage.Luau) {
+                    if (declaration == null) {
+                        EditorGUILayout.HelpBox("This Luau file has no Typescript declaration file!", MessageType.Warning);
+                    }
+                    
+                    EditorGUILayout.Space(10);
+                    GUILayout.Label("Script Details", EditorStyles.boldLabel);
+                    
+                    GUI.enabled = false;
+                    EditorGUILayout.ObjectField("Declaration File", declaration, typeof(DeclarationFile));
+                    GUI.enabled = true;
+                }
+                
+                DrawSourceText();
+            }
+            
+            GUI.enabled = false;
+        }
+    }
+}
