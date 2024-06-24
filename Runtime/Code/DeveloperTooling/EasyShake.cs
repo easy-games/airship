bba319c32affe6779b33dbf80ce1c20565103bc8
Tshadow05 <Tshadow05@gmail.com>
2024-06-24 16:46:03 -0700

    Cleaned up variables of EasyShake and added tooltips

--- Runtime/Code/DeveloperTooling/EasyShake.cs
+++ Runtime/Code/DeveloperTooling/EasyShake.cs
@@ -3,79 +3,118 @@ using UnityEngine;
 [LuauAPI]
 public class EasyShake : MonoBehaviour
 {
-    public float duration = -1;
-    public float movementsPerSecond = 2;
-    public float lerpMod = -1;
-    public Vector3 maxRadius = new Vector3(.5f,.5f,.5f);
-    public Vector3 minRadius = new Vector3(0,0,0);
-    public float positionRadiusMod = 1;
-    public float rotationRadiusMod = 0;
-    
-    public bool minimizeShakeOverTime = false;
-    public bool infinite = false;
-    public bool destroyOnEnd = false;
+    [Header("Timeing")]
+    public bool shakeOnEnable = true;
+
+    [Tooltip("How long to shake. Negative number for infinite")]
+    public float shakeDuration = -1;
+
+    [Space(10)]
+    [Header("Movement")]
+    [Tooltip("The higher the number the faster the object will transition to the next offset. Negative number for instant.")]
+    public float movementLerpMod = 10;
+    [Tooltip("How many times the transform moves each second")]
+    public float movementsPerSecond = 10;
+    [Tooltip("Should the position offsets decrease over time?")]
+    public bool minimizeShakeOverTime = true;
+
+    [Header("Position Randomization")]
+    public Vector3 maxPositionOffset = new Vector3(.5f,.5f,.5f);
+
+    [Header("Rotation Randomization")]
+    public Vector3 maxRotationOffsetAngles = new Vector3(15,15,15);
+
+    [Header("Lifetime")]
+    public bool destroyComponentOnEnd = false;
 
     private float lastMovement = 0;
     private Vector3 originalPosition;
     private Quaternion originalRotation;
     private bool started = false;
-    private float startingDuration = 0;
+    private float shakeStartTime = 0;
+    private float remainingDuration = 0;
     private Vector3 targetPosition;
-    private Vector3 tickRandomOffset = Vector3.zero;
     private Quaternion targetRotation;
 
     private void OnEnable() {
-        originalPosition = transform.localPosition;
-        originalRotation = transform.localRotation;
+        this.SetStartingPosRot(this.transform.position, this.transform.rotation);
         targetPosition = originalPosition;
         targetRotation = originalRotation;
+        if(shakeOnEnable){
+            Shake(shakeDuration);
+        }
     }
 
     // Update is called once per frame
     void LateUpdate() {
-        if (duration > 0 || infinite) {
-            if (!started) {
-                //START
-                startingDuration = duration;
-                started = true;
-            }
-
-            if (!infinite) {
-                duration -= Time.deltaTime;
+        if (remainingDuration != 0) {
+            if (remainingDuration > 0) {
+                remainingDuration = Mathf.Max(0, remainingDuration-Time.deltaTime);
             }
 
             if (Time.time - lastMovement > (1 / movementsPerSecond)) {
                 //TICK
-                Vector3 radius = maxRadius;
+                //Position
+                var positionOffset = maxPositionOffset;
                 if (minimizeShakeOverTime) {
-                    radius = Vector3.Lerp(maxRadius, minRadius, 1-(duration / startingDuration));
+                    positionOffset = Vector3.Lerp(maxPositionOffset, Vector3.zero, (Time.time - shakeStartTime) / shakeDuration);
                 }
+                targetPosition = originalPosition + GetRandomVector3(positionOffset);
 
-                tickRandomOffset.x = Random.Range(-radius.x, radius.x);
-                tickRandomOffset.y = Random.Range(-radius.y, radius.y);
-                tickRandomOffset.z = Random.Range(-radius.z, radius.z);
-                targetPosition = originalPosition + tickRandomOffset * positionRadiusMod;
-                targetRotation = originalRotation * Quaternion.Euler(tickRandomOffset * rotationRadiusMod);
+                //Rotation
+                var rotationOffset = maxRotationOffsetAngles;
+                if (minimizeShakeOverTime) {
+                    rotationOffset = Vector3.Lerp(rotationOffset, Vector3.zero, (Time.time - shakeStartTime) / shakeDuration);
+                }
+                targetRotation = originalRotation * Quaternion.Euler(GetRandomVector3(rotationOffset));
                 
                 lastMovement = Time.time;
             }
             
             //Lerp to target
-            if (lerpMod <= 0) {
+            if (movementLerpMod <= 0) {
                 transform.localPosition = targetPosition;
                 transform.localRotation = targetRotation;
             } else {
                 transform.localPosition
-                    = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * lerpMod);
-                transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime * lerpMod);
+                    = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * movementLerpMod);
+                transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime * movementLerpMod);
             }
         } else if(started){
-            //END
-            started = false;
-            transform.localPosition = originalPosition;
-            if (destroyOnEnd) {
-                Destroy(this);
-            }
+            StopShake();
         }
+    }
+
+    public Vector3 GetRandomVector3(Vector3 maxRange){
+        return new Vector3(
+            Random.Range(-maxRange.x, maxRange.x),
+            Random.Range(-maxRange.y, maxRange.y),
+            Random.Range(-maxRange.z, maxRange.z));
+    }
+
+    public void Shake(float duration){
+        shakeDuration = duration;
+        remainingDuration = shakeDuration;
+        this.shakeStartTime = Time.time;
+        started = true;
+    }
+
+    public void ShakeForever(){
+        Shake(-1);
+    }
+
+    public void StopShake(){
+        started = false;
+        transform.localPosition = originalPosition;
+        transform.localRotation = originalRotation;
+        remainingDuration = 0;
+        if (destroyComponentOnEnd) {
+            Destroy(this);
+        }
+    }
+
+    public void SetStartingPosRot(Vector3 localPosition, Quaternion localRotation){
+        originalPosition = localPosition;
+        originalRotation = localRotation;
     }
 }
