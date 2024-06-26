0bed9d5ed3c9e0bf45b8a8761ebc52fff8c02663
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-06-27 00:00:14 +1200

    Component button

--- Editor/EditorInternal/InspectorExtensions.cs
+++ Editor/EditorInternal/InspectorExtensions.cs
@@ -1,5 +1,9 @@
-﻿namespace Editor.EditorInternal {
+﻿using UnityEditor;
+
+namespace Editor.EditorInternal {
     public class InspectorExtensions {
-        
+        public static UnityEditor.Editor GetFirstNonImportInspectorEditor(UnityEditor.Editor[] editor) {
+            return InspectorWindowUtils.GetFirstNonImportInspectorEditor(editor);
+        }
     }
 }
