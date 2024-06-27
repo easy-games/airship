a0c201b5c796c69899318b1446e5bbf2aa5f99a5
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-06-27 14:49:34 +1200

    Fix `InspectorExtensions`

--- Editor/EditorInternal/InspectorExtensions.cs
+++ Editor/EditorInternal/InspectorExtensions.cs
@@ -1,9 +1,12 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
 
+using UnityEditor;
 namespace Editor.EditorInternal {
-    public class InspectorExtensions {
+    public static class InspectorExtensions {
         public static UnityEditor.Editor GetFirstNonImportInspectorEditor(UnityEditor.Editor[] editor) {
             return InspectorWindowUtils.GetFirstNonImportInspectorEditor(editor);
         }
     }
 }
+
+#endif
