901ee21836460b260aa371e1d060484738518988
Luke Chatton <chattonluke@gmail.com>
2024-07-03 18:06:27 -0700

    remove

--- Runtime/Code/LuauAPI/SessionStateAPI.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEditor;
-
-[LuauAPI(LuauContext.Protected)]
-public class SessionStateAPI : BaseLuaAPIClass {
-    public override Type GetAPIType() {
-        return typeof(SessionState);
-    }
-}
