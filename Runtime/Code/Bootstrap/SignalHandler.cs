3c4be2188e37c3d737a3bed9fb8591b34cad2104
Luke Chatton <chattonluke@gmail.com>
2024-07-08 16:10:10 -0700

    Update SignalHandler.cs

--- Runtime/Code/Bootstrap/SignalHandler.cs
+++ Runtime/Code/Bootstrap/SignalHandler.cs
@@ -1,8 +1,9 @@
 using System.Threading;
 using UnityEngine;
-#if UNITY_STANDALONE_LINUX || UNITY_EDITOR_LINUX
+#if UNITY_STANDALONE_LINUX || UNITY_EDITOR_LINUX || true
 using System;
 using System.Collections;
+using Code.Util;
 using Mono.Unix;
 using Mono.Unix.Native;
 using UnityEngine;
@@ -11,22 +12,24 @@ using UnityEngine;
 public class SignalHandler : MonoBehaviour {
     public ServerBootstrap serverBootstrap;
 
-#if UNITY_STANDALONE_LINUX || UNITY_EDITOR_LINUX
+#if UNITY_STANDALONE_LINUX || UNITY_EDITOR_LINUX || true
     void Start() {
         var thread = new Thread(CheckForSignals);
         thread.Start();
     }
 
     private void CheckForSignals() {
-        var signals = new UnixSignal[] {
-            new UnixSignal(Signum.SIGTERM)
-        };
-        int index = UnixSignal.WaitAny(signals, -1);
+        while (true) {
+            var signals = new UnixSignal[] {
+                new UnixSignal(Signum.SIGTERM)
+            };
+            int index = UnixSignal.WaitAny(signals, -1);
 
-        if (index >= 0 && signals[index].IsSet) {
-            Debug.Log("Sigterm.1");
-            StartCoroutine(HandleSigterm());
-            signals[index].Reset();
+            if (index >= 0 && signals[index].IsSet) {
+                Debug.Log("Sigterm.1");
+                UnityMainThreadDispatcher.Instance.Enqueue(HandleSigterm());
+                signals[index].Reset();
+            }
         }
     }
 
