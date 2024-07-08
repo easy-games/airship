b0b8b7ae161669d6575d64792ab7da76991758c0
Luke Chatton <chattonluke@gmail.com>
2024-07-08 16:00:14 -0700

    Update SignalHandler.cs

--- Runtime/Code/Bootstrap/SignalHandler.cs
+++ Runtime/Code/Bootstrap/SignalHandler.cs
@@ -1,6 +1,8 @@
+using System.Threading;
 using UnityEngine;
-#if UNITY_STANDALONE_LINUX || UNITY_EDITOR_LINUX
+#if UNITY_STANDALONE_LINUX || UNITY_EDITOR_LINUX || true
 using System;
+using System.Collections;
 using Mono.Unix;
 using Mono.Unix.Native;
 using UnityEngine;
@@ -9,35 +11,30 @@ using UnityEngine;
 public class SignalHandler : MonoBehaviour {
     public ServerBootstrap serverBootstrap;
 
-#if UNITY_STANDALONE_LINUX || UNITY_EDITOR_LINUX
-    // private UnixSignal[] signals;
-    //
-    // void Start() {
-    //     signals = new UnixSignal[] {
-    //         new UnixSignal(Signum.SIGTERM)
-    //     };
-    //
-    //     StartCoroutine(CheckForSignals());
-    // }
-    //
-    // private System.Collections.IEnumerator CheckForSignals() {
-    //     while (true) {
-    //         int index = UnixSignal.WaitAny(signals, -1);
-    //
-    //         if (index >= 0 && signals[index].IsSet) {
-    //             HandleSigterm();
-    //             signals[index].Reset();
-    //         }
-    //
-    //         yield return null;
-    //     }
-    // }
-    //
-    // private void HandleSigterm() {
-    //     Debug.Log("SIGTERM received. Performing cleanup.");
-    //     // Perform your cleanup here
-    //     serverBootstrap.InvokeOnProcessExit();
-    //     Application.Quit();
-    // }
+#if UNITY_STANDALONE_LINUX || UNITY_EDITOR_LINUX || true
+    void Start() {
+        var thread = new Thread(new ThreadStart(CheckForSignals));
+        thread.Start();
+    }
+
+    private void CheckForSignals() {
+        var signals = new UnixSignal[] {
+            new UnixSignal(Signum.SIGTERM)
+        };
+        int index = UnixSignal.WaitAny(signals, -1);
+
+        if (index >= 0 && signals[index].IsSet) {
+            StartCoroutine(HandleSigterm());
+            signals[index].Reset();
+        }
+    }
+
+    private IEnumerator HandleSigterm() {
+        Debug.Log("SIGTERM received. Performing cleanup.");
+        // Perform your cleanup here
+        serverBootstrap.InvokeOnProcessExit();
+        yield return null;
+        // Application.Quit();
+    }
 #endif
 }
