10698aaa353ee46aa6253e87bbbf555c4b6e6e64
Luke Chatton <chattonluke@gmail.com>
2024-07-08 15:53:26 -0700

    disable for now

--- Runtime/Code/Bootstrap/SignalHandler.cs
+++ Runtime/Code/Bootstrap/SignalHandler.cs
@@ -10,34 +10,34 @@ public class SignalHandler : MonoBehaviour {
     public ServerBootstrap serverBootstrap;
 
 #if UNITY_STANDALONE_LINUX || UNITY_EDITOR_LINUX
-    private UnixSignal[] signals;
-
-    void Start() {
-        signals = new UnixSignal[] {
-            new UnixSignal(Signum.SIGTERM)
-        };
-
-        StartCoroutine(CheckForSignals());
-    }
-
-    private System.Collections.IEnumerator CheckForSignals() {
-        while (true) {
-            int index = UnixSignal.WaitAny(signals, -1);
-
-            if (index >= 0 && signals[index].IsSet) {
-                HandleSigterm();
-                signals[index].Reset();
-            }
-
-            yield return null;
-        }
-    }
-
-    private void HandleSigterm() {
-        Debug.Log("SIGTERM received. Performing cleanup.");
-        // Perform your cleanup here
-        serverBootstrap.InvokeOnProcessExit();
-        Application.Quit();
-    }
+    // private UnixSignal[] signals;
+    //
+    // void Start() {
+    //     signals = new UnixSignal[] {
+    //         new UnixSignal(Signum.SIGTERM)
+    //     };
+    //
+    //     StartCoroutine(CheckForSignals());
+    // }
+    //
+    // private System.Collections.IEnumerator CheckForSignals() {
+    //     while (true) {
+    //         int index = UnixSignal.WaitAny(signals, -1);
+    //
+    //         if (index >= 0 && signals[index].IsSet) {
+    //             HandleSigterm();
+    //             signals[index].Reset();
+    //         }
+    //
+    //         yield return null;
+    //     }
+    // }
+    //
+    // private void HandleSigterm() {
+    //     Debug.Log("SIGTERM received. Performing cleanup.");
+    //     // Perform your cleanup here
+    //     serverBootstrap.InvokeOnProcessExit();
+    //     Application.Quit();
+    // }
 #endif
 }
