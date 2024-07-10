88cbfe0d5ad58885f655e9e8f8d0dea30f498eee
Luke Chatton <chattonluke@gmail.com>
2024-07-09 19:10:25 -0700

    Update ArchiveAndCompressBundles.cs

--- ScriptableBuildPipeline/Editor/Tasks/ArchiveAndCompressBundles.cs
+++ ScriptableBuildPipeline/Editor/Tasks/ArchiveAndCompressBundles.cs
@@ -292,7 +292,11 @@ namespace UnityEditor.Build.Pipeline.Tasks
                     } else if (!AirshipScriptableBuildPipelineConfig.IsBuildingPackage(bundleName)) {
                         if (bundleName.StartsWith("@")) continue;
                     }
-                    if (pair.Value.resourceFiles == null) {
+
+                    try {
+                        Debug.Log("resources count: " + pair.Value.resourceFiles.Count);
+                    } catch (Exception e) {
+                        Debug.Log("skipping bundle " + bundleName);
                         continue;
                     }
                     // airship end
