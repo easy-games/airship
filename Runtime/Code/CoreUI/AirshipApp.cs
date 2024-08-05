056fbd9c3fff6779d8e212caf659f501775bf518
Luke Chatton <chattonluke@gmail.com>
2024-08-05 13:23:44 -0700

    prod api key

--- Runtime/Code/CoreUI/AirshipApp.cs
+++ Runtime/Code/CoreUI/AirshipApp.cs
@@ -1,3 +1,7 @@
 public class AirshipApp {
+#if AIRSHIP_STAGING
     public static string firebaseApiKey = "AIzaSyB04k_2lvM2VxcJqLKD6bfwdqelh6Juj2o";
+#else
+    public static string firebaseApiKey = "AIzaSyAYw0C18Mt3wijT0ZHKGcS7zVdaPlR_sGI";
+#endif
 }
