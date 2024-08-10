11789c0552f4301e9d1de9477195bb5fa670f342
Luke Chatton <chattonluke@gmail.com>
2024-08-09 20:35:27 -0700

    fix

--- Editor/EditorInternal/LogExtensions.cs
+++ Editor/EditorInternal/LogExtensions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 
 namespace Editor.EditorInternal {
     public static class LogExtensions {
@@ -7,3 +8,4 @@ namespace Editor.EditorInternal {
         }
     }
 }
+#endif
