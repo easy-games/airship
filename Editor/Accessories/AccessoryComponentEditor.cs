7c764cb024ed6c25d516a0cf84436b27d040b45a
Tshadow05 <Tshadow05@gmail.com>
2024-08-14 15:37:37 -0700

    Added tool to auto create items and fill their server class IDs

--- Editor/Accessories/AccessoryComponentEditor.cs
+++ Editor/Accessories/AccessoryComponentEditor.cs
@@ -1,10 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using Editor.Accessories;
 using System.Linq;
 using Code.Player.Accessories;
+using Code.Platform.Server;
+using Code.Platform.Shared;
+using Editor.Auth;
+using System.Threading.Tasks;
 
 [CustomEditor(typeof(AccessoryComponent))]
 public class AccessoryComponentEditor : UnityEditor.Editor {
@@ -16,23 +18,90 @@ public class AccessoryComponentEditor : UnityEditor.Editor {
         foldout = accessoryComponent.bodyMask > 0;
     }
 
-    [MenuItem("Airship/Internal/UpdateClassIds")]
-    public static void UpdateIds() {
+    [MenuItem("Airship/Avatar/Generate Avatatar Items On Server")]
+    public static void CreateAvatarItemsOnServer() {
+        CreateAllAvatarItems();
+    }
+
+    private static async Task CreateAllAvatarItems(){
+#if AIRSHIP_STAGING 
+            var staging = true;
+#else
+            var staging = false;
+#endif
+
         var collection = AssetDatabase.LoadAssetAtPath<AvatarAccessoryCollection>(
             "Assets/AirshipPackages/@Easy/Core/Prefabs/Accessories/AvatarItems/EntireAvatarCollection.asset");
         if (!collection) {
             Debug.LogError("Failed to find collection.");
             return;
         }
+
+        //Find Organization
+        var orgResourceId = staging ? "6536df9f3843ac629cf3b8b1" : "6b62d6e3-9d74-449c-aeac-b4feed2012b1";
+
+        bool first = true;
+        //Find any missing class ID's
         foreach (var accessory in collection.accessories) {
-            accessory.serverClassId = "";
-            EditorUtility.SetDirty(accessory);
+            if(!first){
+                //break;
+            }
+            first = false;
+            var classId = staging ? accessory.serverClassIdStaging : accessory.serverClassId;
+            if(string.IsNullOrEmpty(classId)){
+                var result = await GenerateServerItem(orgResourceId, accessory.name, staging);
+                if(staging){
+                    accessory.serverClassIdStaging = result;
+                }else{
+                    accessory.serverClassId = result;
+                }
+                EditorUtility.SetDirty(accessory);
+            }
         }
+        first = true;
         foreach (var accessory in collection.faces) {
-            accessory.serverClassId = "";
-            EditorUtility.SetDirty(accessory);
+            if(!first){
+                //break;
+            }
+            first = false;
+            var classId = staging ? accessory.serverClassIdStaging : accessory.serverClassId;
+            if(string.IsNullOrEmpty(classId)){
+                var result = await GenerateServerItem(orgResourceId, accessory.name, staging);
+                if(staging){
+                    accessory.serverClassIdStaging = result;
+                }else{
+                    accessory.serverClassId = result;
+                }
+                EditorUtility.SetDirty(accessory);
+            }
         }
         AssetDatabase.SaveAssets();
+    }
+
+    //Creates an item on the server and returns the class ID
+    private static async Task<string> GenerateServerItem(string orgResourceId, string accName, bool staging){
+        Debug.Log("Creating item on server " + (staging ? "STAGING" : "PRODUCTION") + ": " + accName);
+        
+		var res = await AirshipInventoryServiceBackend.CreateItem(orgResourceId, new AccessoryClassInput(){
+            name = accName,
+            description = "Avatar Item",
+            @default = true,
+            imageId = "c0e07e88-09d4-4962-b42d-7794a7ad4cb2",//"f49100a9-8279-4a96-9366-807ee22da848",
+        });
+
+        if(res.success && res.data.Length > 0){
+            Debug.Log("Parsing json: " + res.data);
+            var accData = JsonUtility.FromJson<AccessoryClass>(res.data);
+            if(accData != null){
+                Debug.Log("Created item: " + accData.name + " id: " + accData.classId);
+                return accData.classId;
+            }else{
+                Debug.LogError("Unable to parse json: " + res.data);
+            }
+        }else {
+            Debug.LogError("Unable to create item: " + accName + " error: " + res.error);
+        }
+        return "";
     }
 
     public override void OnInspectorGUI() {
