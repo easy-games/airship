0e94d4ea20fc3dc080778bcc18c78f269a5e6d30
Luke Chatton <chattonluke@gmail.com>
2024-08-15 12:45:04 -0700

    Update NetworkManager.cs

--- ThirdParty/Mirror/Core/NetworkManager.cs
+++ ThirdParty/Mirror/Core/NetworkManager.cs
@@ -794,9 +794,9 @@ namespace Mirror
             NetworkClient.RegisterHandler<NotReadyMessage>(OnClientNotReadyMessageInternal, false);
             NetworkClient.RegisterHandler<SceneMessage>(OnClientSceneInternal, false);
 
-            this.LogAppOnly("Register client messages. playerPrefab=" + (playerPrefab != null ? playerPrefab.name : "null"));
+            // this.LogAppOnly("Register client messages. playerPrefab=" + (playerPrefab != null ? playerPrefab.name : "null"));
             if (playerPrefab != null) {
-                this.LogAppOnly("Registering player prefab.");
+                // this.LogAppOnly("Registering player prefab.");
                 NetworkClient.RegisterPrefab(playerPrefab);
             }
 
