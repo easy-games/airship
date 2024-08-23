33363268b0dbaf20041ec7f09f028a76b11055e3
Luke Chatton <chattonluke@gmail.com>
2024-08-23 14:49:13 -0700

    Update CharacterMovement.cs

--- Runtime/Code/Player/Character/CharacterMovement.cs
+++ Runtime/Code/Player/Character/CharacterMovement.cs
@@ -983,7 +983,7 @@ namespace Code.Player.Character {
 
 		public void SetDebugFlying(bool flying){
 			if (!this.moveData.allowDebugFlying) {
-				Debug.LogError("Unable to fly from console when allowFlying is false. Set this characters CharacterMovementData to allow flying if needed");
+				// Debug.LogError("Unable to fly from console when allowFlying is false. Set this characters CharacterMovementData to allow flying if needed");
 				return;
 			}
 			SetFlying(flying);
