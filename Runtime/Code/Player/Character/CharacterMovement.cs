da71d17062a74a82072f75db7fa58152884b72c7
Tshadow05 <Tshadow05@gmail.com>
2024-08-27 16:43:23 -0700

    Another change to impulse movement

--- Runtime/Code/Player/Character/CharacterMovement.cs
+++ Runtime/Code/Player/Character/CharacterMovement.cs
@@ -661,7 +661,8 @@ namespace Code.Player.Character {
 				
 				//Don't move character in direction its already moveing
 				//Positive dot means we are already moving in this direction. Negative dot means we are moving opposite of velocity.
-				var dirDot = Mathf.Clamp01(1-Vector3.Dot(flatVelocity/ currentSpeed, characterMoveVelocity/ currentSpeed));
+				var rawDot = Vector3.Dot(flatVelocity/ currentSpeed, characterMoveVelocity/ currentSpeed);
+				var dirDot = Mathf.Clamp01(1-rawDot);
 				
 				if(useExtraLogging){
 					print("old vel: " + currentVelocity + " new vel: " + newVelocity + " move dir: " + characterMoveVelocity + " Dir dot: " + dirDot + " currentSpeed: " + currentSpeed + " grounded: " + grounded + " canJump: " + canJump + " didJump: " + didJump);
@@ -687,7 +688,7 @@ namespace Code.Player.Character {
 				}else{
 					//dirDot = dirDot - 1 / 2;
 					//clampedIncrease *= -Mathf.Min(0, dirDot-1);
-					newVelocity += clampedIncrease * dirDot;
+					newVelocity += characterMoveVelocity * dirDot * deltaTime * 2;
 				}
 				//characterMoveVelocity = clampedIncrease;
 				// if(Mathf.Abs(newVelocity.x) < Mathf.Abs(characterMoveVelocity.x)){
