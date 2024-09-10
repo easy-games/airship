050493a21b5bc9cb19205238956a9010fcbca29d
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-09-11 11:51:25 +1200

    Fix importer

--- Editor/TypescriptServices/Editor/TypescriptImporterEditor.cs
+++ Editor/TypescriptServices/Editor/TypescriptImporterEditor.cs
@@ -19,13 +19,8 @@ namespace Airship.Editor {
         public AirshipScriptable script;
         public IEnumerable<AirshipScriptable> scripts;
 
-        public override VisualElement CreateInspectorGUI() {
-            ApplyRevertGUI();
-            return base.CreateInspectorGUI();
-        }
-
         public override void OnEnable() {
-           
+            base.OnEnable();
             
             if (assetTargets.Length > 1) {
                 scripts = assetTargets.Select(target => target as AirshipScriptable);
@@ -34,68 +29,64 @@ namespace Airship.Editor {
                 script = assetTarget as AirshipScriptable;
                 importer = target as TypescriptImporter;
             }
-            
-            base.OnEnable();
         }
 
         public override void OnDisable() {
             scripts = null;
             script = null;
             importer = null;
-            
             base.OnDisable();
         }
 
-        // public override void OnInspectorGUI() {
-        //     importer.scriptType = (ScriptType) EditorGUILayout.EnumPopup("Script Type", importer.scriptType);
-        //     if (GUI.changed) {
-        //         var value = this.serializedObject.FindProperty("scriptType");
-        //         if (value != null) {
-        //             value.enumValueIndex = (int) importer.scriptType;
-        //         }
-        //     }
-        //
-        //     ApplyRevertGUI();
-        // }
+        public override void OnInspectorGUI() {
+            importer.scriptType = (ScriptType) EditorGUILayout.EnumPopup("Script Type", importer.scriptType);
+            if (GUI.changed) {
+                var value = this.serializedObject.FindProperty("scriptType");
+                if (value != null) {
+                    value.enumValueIndex = (int) importer.scriptType;
+                }
+            }
+        
+            ApplyRevertGUI();
+        }
+        
+        protected override bool needsApplyRevert => true;
 
-        // private bool canApplyRevert = false;
-        // protected override bool needsApplyRevert => true;
-
-        // protected override void OnHeaderGUI() {
-        //     var rect = EditorGUILayout.GetControlRect(false, 50, "IN BigTitle");
-        //     
-        //     if (!AssetIcon) {
-        //         AssetIcon = AssetDatabase.LoadAssetAtPath<Texture2D>(IconAsset);
-        //     }
-        //     
-        //     GUILayout.BeginHorizontal();
-        //     {
-        //         GUILayout.Space(38f);
-        //     
-        //         GUILayout.BeginVertical();
-        //         {
-        //             
-        //             
-        //             var textureImage = new Rect(rect);
-        //             textureImage.y += 5;
-        //             textureImage.x += 5;
-        //             textureImage.width = 38;
-        //             textureImage.height = 38;
-        //             GUI.Label(textureImage, AssetIcon);
-        //
-        //             rect.x += 45;
-        //             if (scripts != null) {
-        //                 EditorGUI.LabelField(rect, "Airship Script Assets", EditorStyles.boldLabel);
-        //             }
-        //             else {
-        //                 EditorGUI.LabelField(rect,"Airship Script Asset", EditorStyles.boldLabel);
-        //             }
-        //         }
-        //         GUILayout.EndVertical();
-        //     }
-        //     GUILayout.EndHorizontal();
-        //     // base.OnHeaderGUI();
-        // }
+        protected override void OnHeaderGUI() {
+            var rect = EditorGUILayout.GetControlRect(false, 50, "IN BigTitle");
+            
+            if (!AssetIcon) {
+                AssetIcon = AssetDatabase.LoadAssetAtPath<Texture2D>(IconAsset);
+            }
+            
+            GUILayout.BeginHorizontal();
+            {
+                GUILayout.Space(38f);
+            
+                GUILayout.BeginVertical();
+                {
+                    
+                    
+                    var textureImage = new Rect(rect);
+                    textureImage.y += 5;
+                    textureImage.x += 5;
+                    textureImage.width = 38;
+                    textureImage.height = 38;
+                    GUI.Label(textureImage, AssetIcon);
+        
+                    rect.x += 45;
+                    if (scripts != null) {
+                        EditorGUI.LabelField(rect, "Airship Script Assets", EditorStyles.boldLabel);
+                    }
+                    else {
+                        EditorGUI.LabelField(rect,"Airship Script Asset", EditorStyles.boldLabel);
+                    }
+                }
+                GUILayout.EndVertical();
+            }
+            GUILayout.EndHorizontal();
+            // base.OnHeaderGUI();
+        }
     }
 
     [CanEditMultipleObjects]
