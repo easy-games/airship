1cbae7e18082d3f200db9c7f28b7327ea39ed278
Luke Chatton <chattonluke@gmail.com>
2024-09-23 22:26:14 -0700

    Squashed commit of the following:
    commit 90333951acadb83c4f909071482f59a0380a26a7
    Author: Luke Chatton <chattonluke@gmail.com>
    Date:   Mon Sep 23 22:26:02 2024 -0700
    
        force close works!
    
    commit 736e778441acdcb40a2664b7dbe5d305faeaf2e7
    Author: Luke Chatton <chattonluke@gmail.com>
    Date:   Mon Sep 23 22:13:29 2024 -0700
    
        start on force close

---
+++ Runtime/Code/CoreUI/Components/ForceClose.cs
@@ -0,0 +1,52 @@
+using System;
+using ElRaccoone.Tweens;
+using ElRaccoone.Tweens.Core;
+using TMPro;
+using UnityEngine;
+
+namespace Code.CoreUI.Components {
+    public class ForceClose : MonoBehaviour {
+        public RectTransform container;
+        public RectTransform fill;
+        public TMP_Text text;
+        [NonSerialized] private float holdTimeRequired = 2.2f;
+
+        [NonSerialized] private float holdTime = 0f;
+        [NonSerialized] private ITween tween;
+        [NonSerialized] private bool isShown = false;
+        [NonSerialized] private bool disconnected = false;
+
+        private void Update() {
+            if (Input.GetKey(KeyCode.Escape)) {
+                if (!this.isShown && this.holdTime >= 0.5f) {
+                    this.isShown = true;
+                    if (this.tween != null) {
+                        this.tween.Cancel();
+                        this.tween = null;
+                    }
+
+                    this.tween = NativeTween.AnchoredPositionY(this.container, -10f, 0.3f).SetEaseQuadOut();
+                }
+                this.holdTime += Time.deltaTime;
+
+                float fillAmount = Math.Min(this.holdTime / this.holdTimeRequired, 1);
+                this.fill.anchorMax = new Vector2(fillAmount, this.fill.anchorMax.y);
+
+                if (fillAmount >= 1 && !this.disconnected) {
+                    this.disconnected = true;
+                    this.text.text = "Disconnecting...";
+                    TransferManager.Instance.Disconnect();
+                }
+
+                return;
+            }
+
+            // not holding escape
+            if (this.isShown) {
+                this.isShown = false;
+                this.holdTime = 0f;
+                this.tween = NativeTween.AnchoredPositionY(this.container, 71f, 0.3f).SetEaseQuadOut();
+            }
+        }
+    }
+}
