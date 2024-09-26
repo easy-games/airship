ed2d5c92af682d0d4d512765d7065bf7e5a6b369
Luke Chatton <chattonluke@gmail.com>
2024-09-26 11:03:39 -0700

    disable "select local character" for now.

--- Editor/LocalCharacterSelector.cs
+++ Editor/LocalCharacterSelector.cs
@@ -4,25 +4,25 @@ using UnityEditor;
 using UnityEngine;
 using UnityToolbarExtender;
 
-[InitializeOnLoad]
+// [InitializeOnLoad]
 public static class LocalCharacterSelector {
-    static LocalCharacterSelector() {
-        ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
-    }
+    // static LocalCharacterSelector() {
+    //     ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+    // }
 
-    private static void OnToolbarGUI() {
-        if (!Application.isPlaying || !RunCore.IsClient()) return;
-        
-        GUILayout.FlexibleSpace();
-        if (GUILayout.Button(new GUIContent("Select Local Character", "Select the local entity"),
-                ToolbarStyles.LocalCharacterButtonStyle)) {
-            var ids = Object.FindObjectsByType<NetworkIdentity>(FindObjectsSortMode.None);
-            foreach (var id in ids) {
-                if (id.isOwned) {
-                    Selection.activeGameObject = id.gameObject;
-                    break;
-                }
-            }
-        }
-    }
+    // private static void OnToolbarGUI() {
+    //     if (!Application.isPlaying || !RunCore.IsClient()) return;
+    //
+    //     // GUILayout.FlexibleSpace();
+    //     // if (GUILayout.Button(new GUIContent("Select Local Character", "Select the local entity"),
+    //     //         ToolbarStyles.LocalCharacterButtonStyle)) {
+    //     //     var ids = Object.FindObjectsByType<NetworkIdentity>(FindObjectsSortMode.None);
+    //     //     foreach (var id in ids) {
+    //     //         if (id.isOwned) {
+    //     //             Selection.activeGameObject = id.gameObject;
+    //     //             break;
+    //     //         }
+    //     //     }
+    //     // }
+    // }
 }
