24ba4f61d6bf63e528df1d29590b16198ebcf15b
Luke Chatton <chattonluke@gmail.com>
2024-10-01 13:51:30 -0700

    better windows scroll config

--- Runtime/Code/Components/AirshipScrollRectConfig.cs
+++ Runtime/Code/Components/AirshipScrollRectConfig.cs
@@ -3,14 +3,16 @@ using UnityEngine.UI;
 
 namespace Code.Components {
     public class AirshipScrollRectConfig : MonoBehaviour {
-        private void Awake() {
+        private void Start() {
             var scrollRect = GetComponent<ScrollRect>();
+            print("scroll.1");
             if (!scrollRect) return;
+            print("scroll.2");
 
             if (Application.platform == RuntimePlatform.OSXPlayer || Application.platform == RuntimePlatform.OSXEditor) {
                 scrollRect.scrollSensitivity = 15f;
-            } else if (Application.platform is RuntimePlatform.WindowsPlayer or RuntimePlatform.LinuxPlayer) {
-                scrollRect.scrollSensitivity = 18f;
+            } else if (Application.platform is RuntimePlatform.WindowsPlayer or RuntimePlatform.WindowsEditor or RuntimePlatform.LinuxPlayer or RuntimePlatform.LinuxEditor) {
+                scrollRect.scrollSensitivity = 30f;
             }
         }
     }
