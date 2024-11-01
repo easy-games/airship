c6f81c1aac2e52af907887fb2ac0581320dbfd62
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-11-01 14:10:33 +1300

    Fix build info referencing

--- Editor/AirshipComponentBuildImporter.cs
+++ Editor/AirshipComponentBuildImporter.cs
@@ -7,11 +7,13 @@ namespace Editor {
     [ScriptedImporter(1, "asbuildinfo")]
     public class AirshipComponentBuildImporter : ScriptedImporter {
         public override void OnImportAsset(AssetImportContext ctx) {
+            if (AirshipBuildInfo.PrimaryAssetPath == ctx.assetPath) {
+                AirshipBuildInfo.ClearInstance();
+            }
+            
             var data = File.ReadAllText(ctx.assetPath);
-
             var airshipBuild = ScriptableObject.CreateInstance<AirshipBuildInfo>();
             airshipBuild.data = AirshipBuildData.FromJsonData(data);
-            
             ctx.AddObjectToAsset("build", airshipBuild);
             ctx.SetMainObject(airshipBuild);
         }
