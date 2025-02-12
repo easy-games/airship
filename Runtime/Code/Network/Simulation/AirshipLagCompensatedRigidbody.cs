cf3f59145ac7c4b58e5527c209e569f2b174001f
Jack Schaffner <jackwschaffner@gmail.com>
2025-02-12 13:35:27 -0800

    Update lag comp rigidbody, update send rate

--- Runtime/Code/Network/Simulation/AirshipLagCompensatedRigidbody.cs
+++ Runtime/Code/Network/Simulation/AirshipLagCompensatedRigidbody.cs
@@ -1,6 +1,7 @@
 using System;
 using Mirror;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Code.Network.Simulation
 {
@@ -11,19 +12,30 @@ namespace Code.Network.Simulation
         public Vector3 angularVelocity;
         public Quaternion rotation;
     }
-    
+
     /**
      * This component is used to allow for lag compensation to operate on rigidbodies controlled by the server.
      */
-    public class AirshipLagCompensatedEntity: NetworkBehaviour
+    public class AirshipLagCompensatedRigidbody : NetworkBehaviour
     {
-        [Tooltip("Time in seconds kept in history.")]
-        [Range(0, 1)]
+        [Tooltip("Time kept in history in seconds in seconds.")] [Range(0, 1)]
         public float historySize = 1;
+
+        [Tooltip(
+            "Amount of ticks buffered on the client before rendering. " +
+            "It's important to get this number correct as it controls the additional rollback time required to correctly " +
+            "produce the client's view of the world during lag compensation. It should match the number of physics updates " +
+            "you wait before rendering the object. By default, Airship has 2 updates buffered by Mirror.")]
+        public uint bufferSizeMultiplier = 2;
+
+        [Tooltip("The send rate being used to update this rigidbodies positions. This is used to calculate the " +
+                 "additional rollback time required to produce the client's view of the world during lag compensation. " +
+                 "It should match the number of updates sent per second. Airship uses a default send rate of 50 for Mirror.")]
+        public uint updateSendRate = 50;
 
         private Rigidbody rb;
         private History<RigidbodySnapshot> history;
-        
+
         private void Awake()
         {
             // Lag compensation only occurs on the server, so this component only operates on the server.
@@ -31,7 +43,7 @@ namespace Code.Network.Simulation
             {
                 rb = this.GetComponent<Rigidbody>();
                 var snapshots = historySize / Time.fixedDeltaTime;
-                history = new History<RigidbodySnapshot>((int) Math.Ceiling(snapshots));
+                history = new History<RigidbodySnapshot>((int)Math.Ceiling(snapshots));
                 AirshipSimulationManager.OnCaptureSnapshot += this.CaptureSnapshot;
                 AirshipSimulationManager.OnSetSnapshot += this.SetSnapshot;
                 AirshipSimulationManager.OnLagCompensationCheck += this.LagCompensationCheck;
@@ -71,8 +83,8 @@ namespace Code.Network.Simulation
 
         private void LagCompensationCheck(int clientId, double time, double latency)
         {
-            // TODO: configure buffer time
-            var bufferedTime = time - latency - (3f / Time.fixedDeltaTime);
+            var sendRate = 1f / this.updateSendRate;
+            var bufferedTime = time - latency - (this.bufferSizeMultiplier * sendRate);
             this.SetSnapshot(bufferedTime);
         }
     }
