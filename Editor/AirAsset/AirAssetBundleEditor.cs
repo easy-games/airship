e544edf44441c5723926c9dbe9884b7b79b3fd23
Luke Chatton <chattonluke@gmail.com>
2025-03-03 21:37:24 -0800

    Air Asset Bundles! The cool new way to make your own asset bundle!

---
+++ Editor/AirAsset/AirAssetBundleEditor.cs
@@ -0,0 +1,228 @@
+#if AIRSHIP_INTERNAL
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Code.Accessories.Clothing;
+using Code.AirAsset;
+using Code.Bootstrap;
+using Code.Http.Internal;
+using Code.Platform.Shared;
+using Editor.Accessories.Clothing;
+using Editor.Packages;
+using UnityEditor;
+using UnityEditor.Build.Pipeline;
+using UnityEngine;
+using UnityEngine.Networking;
+using Debug = UnityEngine.Debug;
+using Object = UnityEngine.Object;
+
+namespace Editor.AirAsset {
+    [CustomEditor(typeof(AirAssetBundle))]
+    [CanEditMultipleObjects]
+    public class AirAssetBundleEditor : UnityEditor.Editor {
+        private static string easyOrgId = "6b62d6e3-9d74-449c-aeac-b4feed2012b1";
+        private bool skipBuild = false;
+
+        private void OnEnable() {
+            skipBuild = false;
+        }
+
+        public override void OnInspectorGUI() {
+            base.DrawDefaultInspector();
+
+            GUILayout.Space(20);
+            AirshipEditorGUI.HorizontalLine();
+            GUILayout.Space(20);
+
+            if (GUILayout.Button("Publish")) {
+                this.BuildAllPlatforms();
+            }
+
+            GUILayout.Space(10);
+            this.skipBuild = EditorGUILayout.Toggle("Skip Build", this.skipBuild);
+        }
+
+        private async void BuildAllPlatforms() {
+            var st = Stopwatch.StartNew();
+            bool success = true;
+
+            List<AirshipPlatform> platforms = new();
+            platforms.AddRange(AirshipPlatformUtil.livePlatforms);
+            // platforms.Add(AirshipPlatform.Mac); // debug
+
+            // ********************************* //
+            var airAssetBundle = (AirAssetBundle)this.target;
+
+            if (!CreateAssetBundles.PrePublishChecks()) return;
+
+            string airId = airAssetBundle.airId;
+
+            var contentName = airAssetBundle.name;
+            var contentDescription = "Air Asset Bundle";
+
+            if (string.IsNullOrEmpty(airId)) {
+                // Create new air asset
+                var req = UnityWebRequest.PostWwwForm(
+                    AirshipPlatformUrl.deploymentService + $"/air-assets/owner-type/ORGANIZATION/owner-id/{easyOrgId}",
+                    JsonUtility.ToJson(new AirAssetCreateRequest() {
+                        contentType = "application/airasset",
+                        contentLength = 1,
+                        name = contentName,
+                        description = contentDescription,
+                        platforms = platforms.Select((p) => AirshipPlatformUtil.GetStringName(p)).ToArray(),
+                    }));
+                req.SetRequestHeader("Authorization", "Bearer " + InternalHttpManager.editorAuthToken);
+                await req.SendWebRequest();
+                Debug.Log("create response: " + req.downloadHandler.text);
+                var data = JsonUtility.FromJson<AirAssetCreateResponse>(req.downloadHandler.text);
+                airAssetBundle.airId = data.airAssetId;
+                EditorUtility.SetDirty(this.target);
+                this.SaveChanges();
+                airId = data.airAssetId;
+            }
+
+            List<string> bundlePaths = new();
+            foreach (var platform in platforms) {
+                var path = await this.BuildPlatform(airAssetBundle, platform, airId);
+                if (string.IsNullOrEmpty(path)) {
+                    success = false;
+                    return;
+                }
+
+                bundlePaths.Add(path);
+            }
+
+            if (!success) return;
+
+            // ******************** //
+
+            int bytesCount = 0;
+            for (int i = 0; i < platforms.Count; i++) {
+                var platform = platforms[i];
+                var buildOutputFile = bundlePaths[i];
+
+                // Update air asset
+                var bytes = await File.ReadAllBytesAsync(buildOutputFile);
+                Debug.Log("bytes length: " + bytes.Length + ", path: " + buildOutputFile);
+                bytesCount = bytes.Length;
+                var updateReq = UnityWebRequest.Put(AirshipPlatformUrl.deploymentService + $"/air-assets/{airId}",
+                    JsonUtility.ToJson(new AirAssetCreateRequest() {
+                        contentType = "application/airasset",
+                        contentLength = bytes.Length,
+                        name = contentName,
+                        description = contentDescription,
+                        platforms = platforms.Select((p) => AirshipPlatformUtil.GetStringName(p)).ToArray(),
+                    }));
+                updateReq.SetRequestHeader("Content-Type", "application/json");
+                updateReq.SetRequestHeader("Authorization", "Bearer " + InternalHttpManager.editorAuthToken);
+                updateReq.SetRequestHeader("x-airship-ignore-rate-limit", "true");
+                await updateReq.SendWebRequest();
+                if (updateReq.result != UnityWebRequest.Result.Success) {
+                    Debug.LogError("Failed to update air asset: " + updateReq.downloadHandler.text);
+                    return;
+                }
+                var updateData = JsonUtility.FromJson<AirAssetCreateResponse>(updateReq.downloadHandler.text);
+                var uploadUrl = updateData.urls.UrlFromPlatform(platform);
+
+                // Upload asset bundle
+                {
+                    UnityWebRequest putReq = UnityWebRequest.Put(uploadUrl, bytes);
+                    foreach (var pair in updateData.headers) {
+                        putReq.SetRequestHeader(pair.key, pair.value);
+                    }
+                    putReq.SetRequestHeader("x-airship-ignore-rate-limit", "true");
+
+                    Debug.Log("Uploading asset bundle...");
+                    await putReq.SendWebRequest();
+                    if (putReq.result != UnityWebRequest.Result.Success) {
+                        Debug.LogError(putReq.error);
+                        Debug.LogError(putReq.downloadHandler.text);
+                        return;
+                    }
+                }
+            }
+
+            Debug.Log($"<color=green>Finished building {bundlePaths.Count} asset bundles for all platforms in {st.Elapsed.Seconds} seconds.</color> File size: " + AirshipEditorUtil.GetFileSizeText(bytesCount));
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="platform"></param>
+        /// <returns>Path to built bundle. Empty string if it failed.</returns>
+        private async Task<string> BuildPlatform(AirAssetBundle target, AirshipPlatform platform, string airId) {
+            var st = Stopwatch.StartNew();
+            var manifest = (PlatformGearBundleManifest)this.target;
+
+            var buildOutputFolder = "bundles/airassetbundle/";
+            var buildOutputFile = $"bundles/airassetbundle/{airId}_{AirshipPlatformUtil.GetStringName(platform)}.bundle";
+            var sourceFolderPath = Path.GetRelativePath(".", Directory.GetParent(AssetDatabase.GetAssetPath(manifest))!.FullName);
+
+            List<AssetBundleBuild> builds = CreateAssetBundles.GetPackageAssetBundleBuilds();
+
+            var assetGuids = AssetDatabase.FindAssets("*", new string[] {sourceFolderPath}).ToList();
+            var assetPaths = assetGuids
+                .Select((guid) => AssetDatabase.GUIDToAssetPath(guid))
+                .Where((path) => !path.ToLower().Contains("editor/"))
+                .Where((path) => !path.ToLower().Contains("exclude/"))
+                .Where((p) => !AssetDatabase.IsValidFolder(p))
+                .ToArray();
+            Debug.Log("Resources:");
+            foreach (var path in assetPaths) {
+                Debug.Log("  - " + path);
+            }
+            var addressableNames = assetPaths
+                .Select((p) => p.ToLower())
+                .Select((p) => {
+                    if (p.Contains(target.name + ".asset")) {
+                        // custom name so it's easier to find when loading
+                        return "_AirAssetBundle";
+                    }
+                    return p;
+                })
+                .ToArray();
+            builds.Add(new AssetBundleBuild() {
+                assetBundleName = airId + $"_{AirshipPlatformUtil.GetStringName(platform)}.bundle",
+                assetNames = assetPaths,
+                addressableNames = addressableNames
+            });
+
+            // --------------------- //
+            // Build
+            if (!this.skipBuild) {
+                var buildTarget = AirshipPlatformUtil.ToBuildTarget(platform);
+                var buildTargetGroup = BuildPipeline.GetBuildTargetGroup(buildTarget);
+                if (platform is AirshipPlatform.Windows or AirshipPlatform.Mac or AirshipPlatform.Linux) {
+                    buildTargetGroup = BuildTargetGroup.Standalone;
+                }
+                EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
+                var buildParams = new BundleBuildParameters(
+                    buildTarget,
+                    buildTargetGroup,
+                    buildOutputFolder
+                );
+                buildParams.UseCache = true;
+                EditorUserBuildSettings.switchRomCompressionType = SwitchRomCompressionType.Lz4;
+                buildParams.BundleCompression = BuildCompression.LZ4;
+                var buildContent = new BundleBuildContent(builds);
+
+                AirshipPackagesWindow.buildingPackageId = "game";
+                CreateAssetBundles.buildingBundles = true;
+                AirshipScriptableBuildPipelineConfig.buildingGameBundles = true;
+                ReturnCode returnCode = ContentPipeline.BuildAssetBundles(buildParams, buildContent, out var result);
+                CreateAssetBundles.buildingBundles = false;
+                AirshipScriptableBuildPipelineConfig.buildingGameBundles = false;
+                if (returnCode != ReturnCode.Success) {
+                    Debug.LogError("Failed to build asset bundles. ReturnCode=" + returnCode);
+                    return null;
+                }
+                Debug.Log($"Finished building {platform} in {st.Elapsed.TotalSeconds} seconds.");
+            }
+
+            return buildOutputFile;
+        }
+    }
+}
+#endif
---
+++ Runtime/Code/AirAssetBundle/AirAssetBundle.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Code.Accessories.Clothing;
+using Code.Bootstrap;
+using Code.Platform.Shared;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Code.AirAsset {
+    public class AirAssetBundle : ScriptableObject {
+        public string airId;
+        private AssetBundle assetBundle;
+
+        private static Dictionary<string, Task<bool>> inProgressDownloads = new();
+        private static Dictionary<string, AirAssetBundle> loadedBundles = new();
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        public static void OnReload() {
+            inProgressDownloads.Clear();
+            loadedBundles.Clear();
+        }
+
+        public async Task<Object> LoadAsync(string assetPath) {
+            var res = this.assetBundle.LoadAssetAsync(assetPath);
+            await res;
+            return res.asset;
+        }
+
+        public static async Task<AirAssetBundle> DownloadYielding(string airId) {
+             var platformString = AirshipPlatformUtil.GetStringName(AirshipPlatformUtil.GetLocalPlatform());
+            var url = $"{AirshipPlatformUrl.gameCdn}/airassets/{airId}/{platformString}";
+
+            // Check for in-progress downloads
+            if (inProgressDownloads.TryGetValue(airId, out var task)) {
+                // By the time we've finished awaiting this, the below existing bundle check will handle this.
+                await task;
+            }
+
+            // Check if we already loaded an asset bundle that contains this clothing piece.
+            if (loadedBundles.TryGetValue(airId, out var airBundle)) {
+                return airBundle;
+            }
+
+            var inProgressTask = new TaskCompletionSource<bool>();
+            inProgressDownloads.TryAdd(airId, inProgressTask.Task);
+
+            // Get latest hash
+            Hash128 hash;
+            {
+                var headReq = UnityWebRequest.Head(url);
+                await headReq.SendWebRequest();
+
+                var etag = headReq.GetResponseHeader("ETag");
+                if (string.IsNullOrEmpty(etag)) {
+                    Debug.LogError("Failed to get latest version hash for airId " + airId);
+                    inProgressTask.SetResult(false);
+                    return null;
+                }
+
+                hash = Hash128.Parse(etag);
+            }
+
+            var req = UnityWebRequestAssetBundle.GetAssetBundle(url, hash);
+            await req.SendWebRequest();
+
+            if (req.result != UnityWebRequest.Result.Success) {
+                Debug.LogError("Failed to download clothing bundle.");
+                inProgressTask.SetResult(false);
+                return null;
+            }
+
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(req);
+            // Uncomment to list all assets inside the bundle.
+            // foreach (var asset in bundle.GetAllAssetNames()) {
+            //     Debug.Log("  - " + asset);
+            // }
+            var loadReq = bundle.LoadAssetAsync<AirAssetBundle>("_AirAssetBundle");
+            await loadReq;
+            var airAssetBundle = (AirAssetBundle) loadReq.asset;
+            loadedBundles[airId] = airAssetBundle;
+
+            inProgressTask.SetResult(true);
+            return airAssetBundle;
+        }
+    }
+}
