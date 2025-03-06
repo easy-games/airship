0b7df3faa614c2746a0edf227506b29892581e69
Luke Chatton <chattonluke@gmail.com>
2025-03-06 10:08:50 -0800

    this ended up not being useful but committing commented out anyways

--- Runtime/Code/VoxelWorld/VoxelBlockDefinitionList.cs
+++ Runtime/Code/VoxelWorld/VoxelBlockDefinitionList.cs
@@ -1,8 +1,10 @@
 //write a scriptable object that acts as a container and editor for VoxelBlockDefines called VoxelBlockDefinion
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 #if UNITY_EDITOR
+using System.IO;
 using UnityEditor;
 #endif
 
@@ -20,17 +22,41 @@ public class VoxelBlockDefinionList : ScriptableObject {
 
 [CustomEditor(typeof(VoxelBlockDefinionList))]
 public class VoxelBlockDefinionListEditor : Editor {
-
- /*   public override void OnInspectorGUI() {
-        VoxelBlockDefinionList list = (VoxelBlockDefinionList)target;
-
-        EditorGUILayout.LabelField("Context", list.context);
-
-        if (GUILayout.Button("Add Block")) {
-            list.blockDefinitions.Add(new VoxelBlockDefinition());
-        }
-
-    }*/
+    // public override void OnInspectorGUI() {
+    //     base.OnInspectorGUI();
+    //
+    //     VoxelBlockDefinionList list = (VoxelBlockDefinionList)target;
+    //
+    //     EditorGUILayout.Space();
+    //
+    //     if (GUILayout.Button("Gather in folder")) {
+    //         var parentPath = Path.GetDirectoryName(AssetDatabase.GetAssetPath(list));
+    //         Debug.Log("Parent path: " + parentPath);
+    //         var blockPaths = AssetDatabase.FindAssets("t:VoxelBlockDefinition", new[] { parentPath }).Select((guid) => AssetDatabase.GUIDToAssetPath(guid)).ToArray();
+    //         Debug.Log($"Found {blockPaths.Length} block definitions.");
+    //         int counter = 0;
+    //         foreach (var path in blockPaths) {
+    //             var blockDefinition = AssetDatabase.LoadAssetAtPath<VoxelBlockDefinition>(path);
+    //             Debug.Log($"Loaded {blockDefinition.name} at path: {path}");
+    //             if (!list.blockDefinitions.Contains(blockDefinition)) {
+    //                 list.blockDefinitions.Add(blockDefinition);
+    //                 counter++;
+    //             }
+    //         }
+    //
+    //         Debug.Log($"Added {counter} missing block definitions.");
+    //         if (counter > 0) {
+    //             EditorUtility.SetDirty(list);
+    //             AssetDatabase.SaveAssets();
+    //         }
+    //     }
+    //
+    //     // EditorGUILayout.LabelField("Context", list.context);
+    //     // if (GUILayout.Button("Add Block")) {
+    //     //     list.blockDefinitions.Add(new VoxelBlockDefinition());
+    //     // }
+    //
+    // }
  
 }
         
