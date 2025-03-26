6d7c82f2295fb5af2185ae801ebb75d8d36753da
Luke Chatton <chattonluke@gmail.com>
2025-03-26 16:27:53 -0700

    Revert "disable vsync on mac"
    This reverts commit 63a24e43cf9f2bfcebf09ed341b3650e1491b2c0.

--- Runtime/Code/Bootstrap/ClientBootstrap.cs
+++ Runtime/Code/Bootstrap/ClientBootstrap.cs
@@ -8,9 +8,6 @@ public class ClientBootstrap : MonoBehaviour
     private void Start()
     {
         if (RunCore.IsClient()) {
-#if UNITY_STANDALONE_OSX || true
-            QualitySettings.vSyncCount = 0;
-#endif
             Application.targetFrameRate = (int)Math.Ceiling(Screen.currentResolution.refreshRateRatio.value);
             Application.logMessageReceived += AnalyticsRecorder.RecordLogMessageToAnalytics;
         }
