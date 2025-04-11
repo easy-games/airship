d95da4fd6187ba9d7358fda8a3ae341e76165290
Luke Chatton <chattonluke@gmail.com>
2025-04-11 13:37:09 -0700

    path open fixes for windows

--- Runtime/Code/Util/CrossPlatformFileAPI.cs
+++ Runtime/Code/Util/CrossPlatformFileAPI.cs
@@ -2,8 +2,19 @@ using System.Runtime.InteropServices;
 using System.IO;
 using UnityEngine;
 
-public static class CrossPlatformFileAPI
-{
+public static class CrossPlatformFileAPI {
+#if UNITY_STANDALONE_WIN
+    [DllImport("shell32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    private static extern bool ShellExecuteW(
+        System.IntPtr hwnd,
+        string lpOperation,
+        string lpFile,
+        string lpParameters,
+        string lpDirectory,
+        int nShowCmd
+    );
+#endif
+
 #if UNITY_STANDALONE_OSX
     [DllImport("libc")]
     private static extern int system(string command);
@@ -11,20 +22,20 @@ public static class CrossPlatformFileAPI
 
     public static void OpenPath(string path) {
         if (!Directory.Exists(path) && !File.Exists(path)) {
-            Debug.LogWarning("Directory or file doesn't exist: " + path);
+            Debug.LogWarning($"Directory does not exist: {path}");
             return;
         }
 
 #if UNITY_STANDALONE_WIN
-        path = path.Replace("/", "\\");
-        System.Diagnostics.Process.Start("explorer.exe", $"\"{path}\"");
+        // Use ShellExecute (works in IL2CPP too)
+        ShellExecuteW(System.IntPtr.Zero, "open", path, null, null, 1);
 
 #elif UNITY_STANDALONE_OSX
-        // Uses macOS 'open' command via native system call (works in IL2CPP)
         string openCommand = $"open \"{path}\"";
         system(openCommand);
+
 #else
-        Debug.Log("Folder open not supported on this platform.");
+        Debug.LogWarning("OpenFolder is not supported on this platform.");
 #endif
     }
 }
