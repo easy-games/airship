dce9de66452ac25b1c8d7cfef4b9dbc87b99f2c1
Luke Chatton <chattonluke@gmail.com>
2025-04-17 13:07:11 -0700

    adjust to head

--- Runtime/Code/Player/OcclusionCam.cs
+++ Runtime/Code/Player/OcclusionCam.cs
@@ -67,17 +67,35 @@ namespace Code.Player {
             // var boxHalfExtents = new Vector3(_projectionX * 0.02f, _projectionY * 0.02f, 0.01f);
             // var boxHalfExtents = new Vector3(0.25f, 0.15f, 0f);
 
+            var preDir = (targetPosition - characterPosition).normalized;
+            var preDirDistance = Vector3.Distance(targetPosition, characterPosition);
+            var mainDistanceMod = 1f;
+
             // If cam is too far above attach pos snap up
-            // if (this.adjustToHead && targetPosition.y - camPos.y > this.adjustToHeadHeightThreshold) {
-            //     distance /= (targetPosition.y - camPos.y) / this.adjustToHeadHeightThreshold;
+            if (this.adjustToHead) {
+                float pitch = t.eulerAngles.x;
+                if (pitch > 90f) {
+                    pitch -= 360;
+                }
+                if (pitch <= -45f) {
+                    float alpha = Mathf.Clamp01(1 - (pitch + 45f) / -40f);
+                    // print($"alpha: {alpha}");
+                    targetPosition = Vector3.Lerp(characterPosition, targetPosition, alpha);
+                    mainDistanceMod = alpha;
+                }
+            }
+            // if (this.adjustToHead && yDiff > this.adjustToHeadHeightThreshold) {
+            //     float alpha = 1 / yDiff / this.adjustToHeadHeightThreshold;
+            //     print($"alpha: {alpha}");
+            //     // targetPosition = Vector3.Lerp(characterPosition, targetPosition, alpha);
+            //     // mainDistanceMod = alpha;
             // }
 
             // Pre: Raycast from character to target position to prevent target being in a wall
-            var preDir = targetPosition - characterPosition;
-            Debug.DrawLine(characterPosition, characterPosition + preDir, Color.yellow);
+            Debug.DrawLine(characterPosition, characterPosition + preDir * preDirDistance, Color.yellow);
             // bool preHit = Physics.BoxCast(characterPosition, boxHalfExtents, preDir.normalized, out RaycastHit preHitInfo, t.rotation, preDir.magnitude, mask,
             //     QueryTriggerInteraction.Ignore);
-            bool preHit = Physics.Raycast(characterPosition, preDir.normalized, out RaycastHit preHitInfo, preDir.magnitude, mask,
+            bool preHit = Physics.Raycast(characterPosition, preDir.normalized, out RaycastHit preHitInfo, preDirDistance, mask,
                 QueryTriggerInteraction.Ignore);
             if (preHit) {
                 GizmoUtils.DrawSphere(preHitInfo.point, 0.03f, Color.yellow);
@@ -90,13 +108,13 @@ namespace Code.Player {
             Debug.DrawLine(targetPosition, targetPosition + mainDir, Color.blue);
             // bool mainHit = Physics.BoxCast(targetPosition - mainDir.normalized * 0.1f, boxHalfExtents, mainDir.normalized, out RaycastHit mainHitInfo,
             //     t.rotation, mainDir.magnitude + 0.05f, mask, QueryTriggerInteraction.Ignore);
-            bool mainHit = Physics.Raycast(targetPosition - mainDir.normalized * 0.1f, mainDir.normalized, out RaycastHit mainHitInfo, mainDir.magnitude + 0.05f, mask, QueryTriggerInteraction.Ignore);
+            bool mainHit = Physics.Raycast(targetPosition, mainDir.normalized, out RaycastHit mainHitInfo, mainDir.magnitude * mainDistanceMod, mask, QueryTriggerInteraction.Ignore);
             if (mainHit) {
                 GizmoUtils.DrawSphere(mainHitInfo.point, 0.03f, Color.blue);
                 newCamPos = mainHitInfo.point - mainDir.normalized * 0.1f;
             } else {
-                newCamPos = targetPosition + mainDir;
-                GizmoUtils.DrawSphere(targetPosition + mainDir, 0.03f, Color.blue);
+                newCamPos = targetPosition + mainDir.normalized * mainDir.magnitude * mainDistanceMod;
+                GizmoUtils.DrawSphere(targetPosition + mainDir.normalized * mainDir.magnitude * mainDistanceMod, 0.03f, Color.blue);
             }
 
             t.position = newCamPos;
