a0a44f3f18b2e485c509d57ee1fe3bab02f19fd2
Luke Chatton <chattonluke@gmail.com>
2025-05-23 15:49:20 -0700

    expose PointerEventData + support EventSystem.RaycastAll()

---
+++ Runtime/Code/LuauAPI/PointerEventDataAPI.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine.EventSystems;
+
+[LuauAPI]
+public class PointerEventDataAPI : BaseLuaAPIClass {
+    public override Type GetAPIType() {
+        return typeof(PointerEventData);
+    }
+}
