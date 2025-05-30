8a368be8336a79c3b7779614a3aaf66329315721
Jonathan Holmes <Vorlias@users.noreply.github.com>
2025-05-31 09:09:48 +1200

    Change to check the target prefab not source for meshfilter etc.

--- Editor/Artifacts/AirshipPrefabUtility.cs
+++ Editor/Artifacts/AirshipPrefabUtility.cs
@@ -6,30 +6,29 @@ namespace Airship.Editor {
     internal static class AirshipPrefabUtility {
         internal static bool FindReconcilablePrefabComponent(AirshipComponent component, out AirshipComponent prefabComponent) {
             var isPrefab = PrefabUtility.IsPartOfAnyPrefab(component);
-            prefabComponent = PrefabUtility.GetCorrespondingObjectFromOriginalSource(component);
-
             if (!isPrefab) {
                 prefabComponent = null;
                 return false;
             }
             
-            if (prefabComponent.script == null) {
-                prefabComponent = null;
-                return false;
-            }
-            
-            var networkIdentity = prefabComponent.gameObject.GetComponentInParent<NetworkIdentity>();
+            var networkIdentity = component.gameObject.GetComponentInParent<NetworkIdentity>();
             if (networkIdentity != null) {
                 prefabComponent = null;
                 return false;
             }
 
-            var meshFilter = prefabComponent.GetComponentsInChildren<MeshFilter>(); // because of 'SendMessage' we can't force reconcile these
+            var meshFilter = component.GetComponentsInChildren<MeshFilter>(); // because of 'SendMessage' we can't force reconcile these
             if (meshFilter.Length > 0) {
                 prefabComponent = null;
                 return false;
             }
-
+            
+            prefabComponent = PrefabUtility.GetCorrespondingObjectFromOriginalSource(component);
+            if (prefabComponent.script == null) {
+                prefabComponent = null;
+                return false;
+            }
+            
             return true;
         }
     }
