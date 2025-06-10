13f492bf70f0b6150f221a19b591a3bb1f8471e8
Luke Chatton <chattonluke@gmail.com>
2025-06-10 09:37:54 -0700

    log org role in "players" devcommand

--- Runtime/Code/Player/PlayerManagerBridge.cs
+++ Runtime/Code/Player/PlayerManagerBridge.cs
@@ -115,7 +115,7 @@ namespace Code.Player {
 				Debug.Log($"Players ({this.players.Count}):");
 				int i = 1;
 				foreach (var player in this.players) {
-					Debug.Log($"  {i}. {player.username} - connectionId: {player.connectionId}, userId: {player.userId}");
+					Debug.Log($"  {i}. {player.username} - connectionId: {player.connectionId}, userId: {player.userId}, orgRole: ${player.orgRoleName}");
 					i++;
 				}
 			}));
