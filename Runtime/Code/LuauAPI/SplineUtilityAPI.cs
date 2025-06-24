6d7516bb79d0ae7c5daa3674e84b5ddaaf61c265
Luke Chatton <chattonluke@gmail.com>
2025-06-24 12:07:02 -0700

    fixes for spline utility generic methods

--- Runtime/Code/LuauAPI/SplineUtilityAPI.cs
+++ Runtime/Code/LuauAPI/SplineUtilityAPI.cs
@@ -1,9 +1,52 @@
 ﻿using System;
+using Unity.Mathematics;
 using UnityEngine.Splines;
 
 [LuauAPI]
 public class SplineUtilityAPI : BaseLuaAPIClass {
     public override Type GetAPIType() {
         return typeof(SplineUtility);
+    }
+
+    public override int OverrideStaticMethod(LuauContext context, IntPtr thread, string methodName, int numParameters,
+        ArraySegment<int> parameterDataPODTypes, ArraySegment<IntPtr> parameterDataPtrs, ArraySegment<int> parameterDataSizes) {
+        
+        if (methodName == "EvaluatePosition") {
+            var spline = (Spline) LuauCore.GetParameterAsObject(0, numParameters, parameterDataPODTypes, parameterDataPtrs,
+                parameterDataSizes, thread);
+
+            var f = LuauCore.GetParameterAsFloat(1, numParameters, parameterDataPODTypes, parameterDataPtrs,
+                parameterDataSizes);
+
+            var result = SplineUtility.EvaluatePosition(spline, f);
+            LuauCore.WritePropertyToThread(thread, result, typeof(float3));
+            return 1;
+        }
+        
+        if (methodName == "EvaluateTangent") {
+            var spline = (Spline) LuauCore.GetParameterAsObject(0, numParameters, parameterDataPODTypes, parameterDataPtrs,
+                parameterDataSizes, thread);
+
+            var f = LuauCore.GetParameterAsFloat(1, numParameters, parameterDataPODTypes, parameterDataPtrs,
+                parameterDataSizes);
+
+            var result = SplineUtility.EvaluateTangent(spline, f);
+            LuauCore.WritePropertyToThread(thread, result, typeof(float3));
+            return 1;
+        }
+        
+        if (methodName == "EvaluateUpVector") {
+            var spline = (Spline) LuauCore.GetParameterAsObject(0, numParameters, parameterDataPODTypes, parameterDataPtrs,
+                parameterDataSizes, thread);
+
+            var f = LuauCore.GetParameterAsFloat(1, numParameters, parameterDataPODTypes, parameterDataPtrs,
+                parameterDataSizes);
+
+            var result = SplineUtility.EvaluateUpVector(spline, f);
+            LuauCore.WritePropertyToThread(thread, result, typeof(float3));
+            return 1;
+        }
+
+        return -1;
     }
 }
