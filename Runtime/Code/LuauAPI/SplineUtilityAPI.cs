4dff2e3423185f0db93484919e2290dc6e750e3b
Luke Chatton <chattonluke@gmail.com>
2025-06-24 11:54:32 -0700

    add SplineUtilityAPI

---
+++ Runtime/Code/LuauAPI/SplineUtilityAPI.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine.Splines;
+
+[LuauAPI]
+public class SplineUtilityAPI : BaseLuaAPIClass {
+    public override Type GetAPIType() {
+        return typeof(SplineUtility);
+    }
+}
