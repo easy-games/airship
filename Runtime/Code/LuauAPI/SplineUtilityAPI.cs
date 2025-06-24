76a28d7503d5d184734b0bbd60551ec11c473b72
Luke Chatton <chattonluke@gmail.com>
2025-06-24 12:14:03 -0700

    expose CurveUtility

--- Runtime/Code/LuauAPI/SplineUtilityAPI.cs
+++ Runtime/Code/LuauAPI/SplineUtilityAPI.cs
@@ -8,45 +8,45 @@ public class SplineUtilityAPI : BaseLuaAPIClass {
         return typeof(SplineUtility);
     }
 
-    public override int OverrideStaticMethod(LuauContext context, IntPtr thread, string methodName, int numParameters,
-        ArraySegment<int> parameterDataPODTypes, ArraySegment<IntPtr> parameterDataPtrs, ArraySegment<int> parameterDataSizes) {
-        
-        if (methodName == "EvaluatePosition") {
-            var spline = (ISpline) LuauCore.GetParameterAsObject(0, numParameters, parameterDataPODTypes, parameterDataPtrs,
-                parameterDataSizes, thread);
-
-            var f = LuauCore.GetParameterAsFloat(1, numParameters, parameterDataPODTypes, parameterDataPtrs,
-                parameterDataSizes);
-
-            var result = SplineUtility.EvaluatePosition(spline, f);
-            LuauCore.WritePropertyToThread(thread, result, typeof(float3));
-            return 1;
-        }
-        
-        if (methodName == "EvaluateTangent") {
-            var spline = (ISpline) LuauCore.GetParameterAsObject(0, numParameters, parameterDataPODTypes, parameterDataPtrs,
-                parameterDataSizes, thread);
-
-            var f = LuauCore.GetParameterAsFloat(1, numParameters, parameterDataPODTypes, parameterDataPtrs,
-                parameterDataSizes);
-
-            var result = SplineUtility.EvaluateTangent(spline, f);
-            LuauCore.WritePropertyToThread(thread, result, typeof(float3));
-            return 1;
-        }
-        
-        if (methodName == "EvaluateUpVector") {
-            var spline = (ISpline) LuauCore.GetParameterAsObject(0, numParameters, parameterDataPODTypes, parameterDataPtrs,
-                parameterDataSizes, thread);
-
-            var f = LuauCore.GetParameterAsFloat(1, numParameters, parameterDataPODTypes, parameterDataPtrs,
-                parameterDataSizes);
-
-            var result = SplineUtility.EvaluateUpVector(spline, f);
-            LuauCore.WritePropertyToThread(thread, result, typeof(float3));
-            return 1;
-        }
-
-        return -1;
-    }
+    // public override int OverrideStaticMethod(LuauContext context, IntPtr thread, string methodName, int numParameters,
+    //     ArraySegment<int> parameterDataPODTypes, ArraySegment<IntPtr> parameterDataPtrs, ArraySegment<int> parameterDataSizes) {
+    //     
+    //     if (methodName == "EvaluatePosition") {
+    //         var spline = (ISpline) LuauCore.GetParameterAsObject(0, numParameters, parameterDataPODTypes, parameterDataPtrs,
+    //             parameterDataSizes, thread);
+    //
+    //         var f = LuauCore.GetParameterAsFloat(1, numParameters, parameterDataPODTypes, parameterDataPtrs,
+    //             parameterDataSizes);
+    //
+    //         var result = SplineUtility.EvaluatePosition(spline, f);
+    //         LuauCore.WritePropertyToThread(thread, result, typeof(float3));
+    //         return 1;
+    //     }
+    //     
+    //     if (methodName == "EvaluateTangent") {
+    //         var spline = (ISpline) LuauCore.GetParameterAsObject(0, numParameters, parameterDataPODTypes, parameterDataPtrs,
+    //             parameterDataSizes, thread);
+    //
+    //         var f = LuauCore.GetParameterAsFloat(1, numParameters, parameterDataPODTypes, parameterDataPtrs,
+    //             parameterDataSizes);
+    //
+    //         var result = SplineUtility.EvaluateTangent(spline, f);
+    //         LuauCore.WritePropertyToThread(thread, result, typeof(float3));
+    //         return 1;
+    //     }
+    //     
+    //     if (methodName == "EvaluateUpVector") {
+    //         var spline = (ISpline) LuauCore.GetParameterAsObject(0, numParameters, parameterDataPODTypes, parameterDataPtrs,
+    //             parameterDataSizes, thread);
+    //
+    //         var f = LuauCore.GetParameterAsFloat(1, numParameters, parameterDataPODTypes, parameterDataPtrs,
+    //             parameterDataSizes);
+    //
+    //         var result = SplineUtility.EvaluateUpVector(spline, f);
+    //         LuauCore.WritePropertyToThread(thread, result, typeof(float3));
+    //         return 1;
+    //     }
+    //
+    //     return -1;
+    // }
 }
