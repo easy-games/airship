e0799a937aa0c1e3929199a2f1488a376be43243
Jonah Nestrick <undecidedapollo@gmail.com>
2025-08-04 16:44:57 -0400

    Remove unused System.Threading import.

--- Runtime/Code/Analytics/ClientFolderUploader.cs
+++ Runtime/Code/Analytics/ClientFolderUploader.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Threading;
 using System.Threading.Tasks;
 using Code.Http.Internal;
 using Code.Http.Public;
