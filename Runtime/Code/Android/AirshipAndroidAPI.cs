a9003e8bf0d7c549c554f3a0b7c2120d5af8877e
Jonathan Holmes <Vorlias@users.noreply.github.com>
2025-08-09 12:11:17 +1200

    Changes

--- Runtime/Code/Android/AirshipAndroidAPI.cs
+++ Runtime/Code/Android/AirshipAndroidAPI.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using UnityEngine;
 
+#if UNITY_ANDROID
+using UnityEngine.Android;
+#endif
+
 public static class AirshipAndroidAPI {
     private const string WINDOW_METHOD_ADD_FLAGS = "addFlags";
     private const string WINDOW_METHOD_CLEAR_FLAGS = "clearFlags";
@@ -9,8 +13,7 @@ public static class AirshipAndroidAPI {
     private const string AIRSHIP_ANDROID_PLAYER_CLASS = "gg.easy.airship.AirshipAndroidPlayer";
 
     public enum AndroidPlayerContext {
-        [Obsolete]
-        Startup,
+        Init,
         Menu,
         Game,
     }
@@ -22,43 +25,51 @@ public static class AirshipAndroidAPI {
             _androidPlayer = new AndroidJavaClass(AIRSHIP_ANDROID_PLAYER_CLASS);
         }
 
-        private AndroidJavaObject androidPlayer {
+        private AndroidJavaObject currentActivity {
             get {
-                if (_androidPlayer == null) _androidPlayer = new AndroidJavaClass(AIRSHIP_ANDROID_PLAYER_CLASS);
-                return _androidPlayer;
+#if UNITY_ANDROID
+                return AndroidApplication.currentActivity;
+#else
+                return null;
+#endif
             }
         }
         
         public void SetContext(AndroidPlayerContext context) {
+            
+            
             switch (context) {
                 case AndroidPlayerContext.Game: {
-                    Screen.fullScreen = true;
+                    Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
                     break;
                 }
                 case AndroidPlayerContext.Menu: {
-                    Screen.fullScreen = false;
+                    Screen.fullScreenMode = FullScreenMode.Windowed;
                     break;
                 }
             }
+
+            var contextInteger = (int) context;
+            currentActivity.Call("setContext", contextInteger);
         }
 
         public void ShowToast(string message) {
-            androidPlayer.Call("showToast", message);
+            currentActivity.Call("showToast", message);
         }
 
-        private static uint ToARGB(Color color) {
-            Color32 c = color;
-            byte[] b = { c.b, c.g, c.r, c.a };
-            return BitConverter.ToUInt32(b, 0);
-        }
+        // private static uint ToARGB(Color color) {
+        //     Color32 c = color;
+        //     byte[] b = { c.b, c.g, c.r, c.a };
+        //     return BitConverter.ToUInt32(b, 0);
+        // }
 
-        public void SetThemeColor(Color navigationColor, Color statusbarColor) {
-            SetThemeColor(ToARGB(navigationColor), ToARGB(statusbarColor));
-        }
-
-        private void SetThemeColor(uint navigationColorARGB, uint statusbarColorARGB) {
-            androidPlayer.Call("setThemeColor", navigationColorARGB, statusbarColorARGB);
-        }
+        // public void SetThemeColor(Color navigationColor, Color statusbarColor) {
+        //     SetThemeColor(ToARGB(navigationColor), ToARGB(statusbarColor));
+        // }
+        //
+        // private void SetThemeColor(uint navigationColorARGB, uint statusbarColorARGB) {
+        //     currentActivity.Call("setThemeColor", navigationColorARGB, statusbarColorARGB);
+        // }
     }
 
     private static AndroidPluginContext _pluginPluginContext;
