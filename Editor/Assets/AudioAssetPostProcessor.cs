28cf7e6e3001dd790754464af77228ce4edbb0fd
Luke Chatton <chattonluke@gmail.com>
2025-08-20 10:47:01 -0700

    disable the new audio asset processor for now

--- Editor/Assets/AudioAssetPostProcessor.cs
+++ Editor/Assets/AudioAssetPostProcessor.cs
@@ -6,30 +6,31 @@ using UnityEngine;
 namespace Editor.Assets {
     public class AudioAssetPostProcessor : AssetPostprocessor {
         private static HashSet<string> processedClips = new HashSet<string>();
-        
-        private void OnPostprocessAudio(AudioClip clip) {
-            var importer = (AudioImporter) assetImporter;
-            if (processedClips.Contains(importer.assetPath)) return; // Already processed
-                
-            importer.loadInBackground = true;
-            
-            var audioImporterSampleSettings = new AudioImporterSampleSettings() {
-                compressionFormat = AudioCompressionFormat.Vorbis,
-                quality = 0.5f,
-            };
-            // For clips over 30s in length we compress in memory on mobile to keep memory lower
-            if (clip.length > 30) {
-                var mobileQuality = new AudioImporterSampleSettings() {
-                    loadType = AudioClipLoadType.CompressedInMemory,
-                    compressionFormat = AudioCompressionFormat.Vorbis,
-                    quality = 0.3f,
-                };
-                importer.SetOverrideSampleSettings(BuildTargetGroup.iOS, mobileQuality);
-                importer.SetOverrideSampleSettings(BuildTargetGroup.Android, mobileQuality);
-            }
-            importer.defaultSampleSettings = audioImporterSampleSettings;
-            processedClips.Add(importer.assetPath);
-            importer.SaveAndReimport();
-        }
+
+        // todo: leaving this disabled for now. These are optimized settings for audio clips.  
+        // private void OnPostprocessAudio(AudioClip clip) {
+        //     var importer = (AudioImporter) assetImporter;
+        //     if (processedClips.Contains(importer.assetPath)) return; // Already processed
+        //
+        //     importer.loadInBackground = true;
+        //
+        //     var audioImporterSampleSettings = new AudioImporterSampleSettings() {
+        //         compressionFormat = AudioCompressionFormat.Vorbis,
+        //         quality = 0.5f,
+        //     };
+        //     // For clips over 30s in length we compress in memory on mobile to keep memory lower
+        //     if (clip.length > 30) {
+        //         var mobileQuality = new AudioImporterSampleSettings() {
+        //             loadType = AudioClipLoadType.CompressedInMemory,
+        //             compressionFormat = AudioCompressionFormat.Vorbis,
+        //             quality = 0.3f,
+        //         };
+        //         importer.SetOverrideSampleSettings(BuildTargetGroup.iOS, mobileQuality);
+        //         importer.SetOverrideSampleSettings(BuildTargetGroup.Android, mobileQuality);
+        //     }
+        //     importer.defaultSampleSettings = audioImporterSampleSettings;
+        //     processedClips.Add(importer.assetPath);
+        //     importer.SaveAndReimport();
+        // }
     }
 }
