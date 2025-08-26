ede22d1b6b2041ccd0fd08af5977dc74d5068432
Luke Chatton <chattonluke@gmail.com>
2025-08-26 14:34:39 -0700

    swap quality level when building clients

--- Editor/BuildMenu.cs
+++ Editor/BuildMenu.cs
@@ -160,6 +160,8 @@ namespace Editor {
             OnBuild();
             CreateAssetBundles.ResetScenes();
 
+            CreateAssetBundles.SwapToQualityLevel("Normal");
+
             UserBuildSettings.architecture = OSArchitecture.x64ARM64;
             PlayerSettings.SplashScreen.show = false;
             PlayerSettings.SetScriptingBackend(NamedBuildTarget.Standalone, ScriptingImplementation.IL2CPP);
@@ -193,6 +195,8 @@ namespace Editor {
 #if UNITY_EDITOR_OSX
             OnBuild();
             CreateAssetBundles.ResetScenes();
+
+            CreateAssetBundles.SwapToQualityLevel("Low");
 
             UserBuildSettings.architecture = OSArchitecture.x64ARM64;
             PlayerSettings.SplashScreen.show = false;
@@ -242,6 +246,8 @@ namespace Editor {
         public static void BuildAndroidClient(AndroidBuildType buildType) {
             var development = buildType == AndroidBuildType.DevelopmentAPK;
             var buildApk = buildType != AndroidBuildType.ReleaseAAB;
+
+            CreateAssetBundles.SwapToQualityLevel("Low");
             
             OnBuild();
             CreateAssetBundles.ResetScenes();
@@ -346,6 +352,8 @@ namespace Editor {
 #if UNITY_EDITOR
             OnBuild();
             CreateAssetBundles.ResetScenes();
+
+            CreateAssetBundles.SwapToQualityLevel("Normal");
 
             PlayerSettings.SplashScreen.show = false;
             PlayerSettings.SetScriptingBackend(NamedBuildTarget.Standalone, ScriptingImplementation.IL2CPP);
