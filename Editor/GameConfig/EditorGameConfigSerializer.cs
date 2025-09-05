4c46c18282de2651d2ee9279bf485c159c1f8925
liamlutton <lutton+gh@me.com>
2025-09-05 10:57:53 -0700

    Save GameConfig on exit edit mode

---
+++ Editor/GameConfig/EditorGameConfigSerializer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+[InitializeOnLoad]
+public class EditorGameConfigSerializer {
+    static EditorGameConfigSerializer() {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+    private static void OnPlayModeStateChanged(PlayModeStateChange stateChange) {
+        if (stateChange == PlayModeStateChange.ExitingEditMode) {
+            try {
+                var gameConfig = GameConfig.Load();
+                
+                // Only Dirty & Save GameConfig if the JSON changes. On my device takes less than 10ms if no changes
+                // were found. If we do have to save this takes around 80ms.
+                var prevHash = JsonUtility.ToJson(gameConfig).GetHashCode();
+                gameConfig.SerializeSettings();
+                if (prevHash != JsonUtility.ToJson(gameConfig).GetHashCode()) {
+                    EditorUtility.SetDirty(gameConfig);
+                    AssetDatabase.SaveAssetIfDirty(gameConfig);   
+                }
+            } catch (Exception ex) {
+                Debug.LogError("Error when copying Unity properties to GameConfig: " + ex);
+            }
+        }
+    }
+}
