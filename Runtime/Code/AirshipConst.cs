53f264a48f95f9df758477fd9fc3829d6b805b94
Stephen Leitnick <sleitnick@gmail.com>
2025-09-27 22:03:07 -0400

    Bump current and min version

--- Runtime/Code/AirshipConst.cs
+++ Runtime/Code/AirshipConst.cs
@@ -2,11 +2,11 @@
 
 namespace Code {
     public static class AirshipConst {
-        public const int playerVersion = 16;
+        public const int playerVersion = 17;
 
         /// <summary>
         /// The server will kick clients that have a playerVersion lower than this value.
         /// </summary>
-        public const int minAcceptedPlayerVersionOnServer = 15;
+        public const int minAcceptedPlayerVersionOnServer = 17;
     }
 }
