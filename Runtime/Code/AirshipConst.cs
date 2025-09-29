6603ad01a119d08f93c4121ad1dad46076d55b86
Luke Chatton <chattonluke@gmail.com>
2025-09-29 11:11:15 -0700

    playerVersion++

--- Runtime/Code/AirshipConst.cs
+++ Runtime/Code/AirshipConst.cs
@@ -3,7 +3,7 @@
 namespace Code {
     [LuauAPI]
     public static class AirshipConst {
-        public const int playerVersion = 16;
+        public const int playerVersion = 17;
 
         /// <summary>
         /// The server will kick clients that have a playerVersion lower than this value.
