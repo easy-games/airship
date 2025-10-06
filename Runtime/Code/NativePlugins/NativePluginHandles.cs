29eebdaec6c9d74f754856d748955e9bc6cf4b50
Stephen Leitnick <sleitnick@gmail.com>
2025-10-06 15:49:50 -0400

    Create EditorPref store for temporary holding of unity interface pointer

--- Runtime/Code/NativePlugins/NativePluginHandles.cs
+++ Runtime/Code/NativePlugins/NativePluginHandles.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Reflection;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace NativePlugins {
 	internal class PluginHandles {
@@ -67,7 +71,7 @@ namespace NativePlugins {
 
 			// Call the UnityPluginLoad plugin function if it exists:
 			if (NativeLibUtil.TryGetDelegate<UnityPluginLoadDelegate>(handle, "UnityPluginLoad", out var loadFn)) {
-				loadFn(UnityInterfaces);
+				loadFn(UnityInterfacesPointerStore.GetUnityInterfacesPointer());
 			}
 			
 			return handle;
@@ -101,20 +105,126 @@ namespace NativePlugins {
 			// up its context object). Since loaded libraries are loaded into the address space
 			// of the application, we'll just let the OS close out our libraries when Unity exits.
 		}
+#endif
+	}
 	
+#if UNITY_EDITOR
+	[InitializeOnLoad]
+	internal class UnityInterfacesPointerStore : IDisposable {
+		private const string EditorPrefAirshipUnityInterfacePointer = "AirshipUnityInterfacePointer";
+		
 		[DllImport("UnityInterfacePlugin", EntryPoint = "GetUnityInterfacesPointer")]
 		private static extern IntPtr GetUnityInterfacesPointerNative();
+		
+		private static IntPtr _unityInterfacesPointerCache;
 
-		private static IntPtr _unityInterfacesPointer;
-		private static IntPtr UnityInterfaces {
-			get {
-				if (_unityInterfacesPointer == IntPtr.Zero) {
-					_unityInterfacesPointer = GetUnityInterfacesPointerNative();
+		static UnityInterfacesPointerStore() {
+			EditorApplication.quitting += () => {
+				using var store = new UnityInterfacesPointerStore();
+				var currentEditorId = EditorAnalyticsSessionInfo.id;
+				store.Remove(currentEditorId);
+			};
+		}
+
+		internal static IntPtr GetUnityInterfacesPointer() {
+			Debug.Log("[NativePluginHandles::GetUnityInterfacesPointer] Called!");
+			
+			// Attempt to get cached version (gets cleared during domain refreshes):
+			if (_unityInterfacesPointerCache != IntPtr.Zero) {
+				Debug.Log("[NativePluginHandles::GetUnityInterfacesPointer] Got from cache");
+				return _unityInterfacesPointerCache;
+			}
+			
+			using var store = new UnityInterfacesPointerStore();
+			var currentEditorId = EditorAnalyticsSessionInfo.id;
+			
+			// Attempt to get pointer from plugin. Returns null after domain refreshes because plugin's UnityPluginLoad
+			// method doesn't get called again:
+			var ptrFromPlugin = GetUnityInterfacesPointerNative();
+			if (ptrFromPlugin != IntPtr.Zero) {
+				Debug.Log("[NativePluginHandles::GetUnityInterfacesPointer] Got from plugin");
+				_unityInterfacesPointerCache = ptrFromPlugin;
+				store.Add(currentEditorId, ptrFromPlugin.ToInt64());
+				return ptrFromPlugin;
+			}
+			
+			Debug.Log("[NativePluginHandles::GetUnityInterfacesPointer] Getting from editor prefs");
+			if (store.TryGet(currentEditorId, out var ptr64)) {
+				return new IntPtr(ptr64);
+			}
+			
+			Debug.LogError("[NativePluginHandles::GetUnityInterfacesPointer] Failed to get pointer from editor prefs");
+			return IntPtr.Zero;
+		}
+
+		private readonly Dictionary<long, long> _editorIdToPtr = new();
+		private bool _modified;
+
+		internal UnityInterfacesPointerStore() {
+			Refresh();
+		}
+
+		private void Refresh() {
+			Debug.Log("[Store::Refresh] Refreshing");
+			_editorIdToPtr.Clear();
+			
+			var editorPref = EditorPrefs.GetString(EditorPrefAirshipUnityInterfacePointer);
+			var editorPtrPairs = new List<string>(editorPref.Split("&"));
+			foreach (var pair in editorPtrPairs) {
+				var editorAndPtr = pair.Split("=", 2);
+				if (editorAndPtr.Length != 2) {
+					continue;
 				}
-				return _unityInterfacesPointer;
+				var editorId = long.Parse(editorAndPtr[0], NumberStyles.HexNumber);
+				var ptr64 = long.Parse(editorAndPtr[1], NumberStyles.HexNumber);
+				if (!_editorIdToPtr.TryAdd(editorId, ptr64)) {
+					Debug.LogWarning($"[NativePluginHandles::Refresh] Pointer already found for given editor id {editorId}");
+					_modified = true;
+				}
 			}
 		}
 
-#endif
+		private void Save() {
+			Debug.Log("[Store::Refresh] Saving maybe");
+			if (!_modified) return;
+			Debug.Log("[Store::Refresh] Saving");
+			
+			var pairs = new List<string>(_editorIdToPtr.Count);
+			foreach (var (editorId, ptr64) in _editorIdToPtr) {
+				pairs.Add($"{editorId:x}={ptr64:x}");
+			}
+			var editorPref = string.Join("&", pairs);
+			EditorPrefs.SetString(EditorPrefAirshipUnityInterfacePointer, editorPref);
+			
+			_modified = false;
+		}
+
+		private bool TryGet(long editorId, out long ptr64) {
+			return _editorIdToPtr.TryGetValue(editorId, out ptr64);
+		}
+
+		private void Add(long editorId, long ptr64) {
+			Debug.Log("[Store::Refresh] Adding maybe");
+			if (_editorIdToPtr.TryGetValue(editorId, out var existingPtr64) && existingPtr64 == ptr64) {
+				return;
+			}
+			
+			_editorIdToPtr[editorId] = ptr64;
+			_modified = true;
+			Debug.Log("[Store::Refresh] Added");
+		}
+
+		private void Remove(long editorId) {
+			Debug.Log("[Store::Refresh] Removing maybe");
+			if (_editorIdToPtr.Remove(editorId)) {
+				Debug.Log("[Store::Refresh] Removed");
+				_modified = true;
+			}
+		}
+
+		public void Dispose() {
+			Save();
+		}
 	}
+#endif
 }
