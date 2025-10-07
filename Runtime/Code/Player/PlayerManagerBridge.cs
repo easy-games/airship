cd32d3cd08b1c9ec663e6517de3435be32b2c240
Luke Chatton <chattonluke@gmail.com>
2025-10-07 12:02:25 -0700

    add playerVersion to PlayerInfo

--- Runtime/Code/Player/PlayerManagerBridge.cs
+++ Runtime/Code/Player/PlayerManagerBridge.cs
@@ -173,7 +173,7 @@ namespace Code.Player {
             var go = Instantiate(playerPrefab, Instance.transform.parent);
 
             var playerInfo = go.GetComponent<PlayerInfo>();
-            playerInfo.Init(connectionId, userId, username, profilePictureId, string.Empty, string.Empty);
+            playerInfo.Init(connectionId, userId, username, profilePictureId, string.Empty, string.Empty, AirshipConst.playerVersion);
 
             // var identity = go.GetComponent<NetworkIdentity>();
             NetworkServer.Spawn(go);
@@ -217,7 +217,7 @@ namespace Code.Player {
 // 				Debug.Log($"Initializing Player as {userData.username} owned by " + conn);
 // #endif
                 playerInfo.Init(conn.connectionId, userData.uid, userData.username, userData.profileImageId,
-                    userData.orgRoleName, userData.fullTransferPacket);
+                    userData.orgRoleName, userData.fullTransferPacket, userData.playerVersion);
             } else {
 #if UNITY_SERVER || true
                 Debug.Log("Missing UserData for " + conn);
