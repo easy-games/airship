cc395e3fe5c23538a47ea82f064b64cd6aa7d5c0
Jack Schaffner <jackwschaffner@gmail.com>
2025-10-09 11:42:04 -0700

    Disconnect when hitting max uncompressed transfer limit

--- Runtime/Code/Network/Net.cs
+++ Runtime/Code/Network/Net.cs
@@ -30,69 +30,15 @@ namespace Assets.Luau.Network {
         private readonly Dictionary<int, ThrottleTrack> _throttle = new();
 
         // Client-to-server data throttle parameters:
-        private const float ThrottleResetPeriod = 0.25f;
+        private const float ThrottleResetPeriod = 1f;
         private const ulong MaxBytesPerSecond = 1024 * 1024 * 50; // 50 MB
         private const ulong MaxBytesAtOnce = 1024 * 1024 * 5; // 5 MB
         
         private const ulong MaxBytesPerPeriod = (ulong)((double)MaxBytesPerSecond * (double)ThrottleResetPeriod);
 
-        // Max items that can end up in the throttle queue:
-        private const int MaxQueueSize = 1024;
-        
-        // Throttled requests get put into a queue:
-        private readonly Dictionary<int, Queue<NetBroadcast>> _throttleQueue = new();
-        private readonly List<int> _throttleQueueRemovals = new();
-        
-#if UNITY_SERVER || UNITY_EDITOR
-        private void Update() {
-	        if (_throttleQueue.Count == 0) {
-		        return;
-	        }
-	        TryFlushAllThrottleQueues();
-        }
-#endif
-	    
-        private void TryFlushAllThrottleQueues() {
-	        var now = Time.realtimeSinceStartup;
-	        
-	        foreach (var (connId, queue) in _throttleQueue) {
-		        TryFlushThrottleQueue(now, connId, queue);
-
-		        if (queue.Count == 0) {
-			        _throttleQueueRemovals.Add(connId);
-		        }
-	        }
-
-	        if (_throttleQueueRemovals.Count > 0) {
-		        foreach (var connId in _throttleQueueRemovals) {
-			        _throttleQueue.Remove(connId);
-		        }
-		        _throttleQueueRemovals.Clear();
-	        }
-        }
-
-        private void TryFlushThrottleQueue(float now, int connId, Queue<NetBroadcast> queue) {
-		    var throttle = _throttle[connId];
-		    if (now >= throttle.nextClear) {
-			    throttle.dataAmount = 0;
-			    throttle.nextClear = now + ThrottleResetPeriod;
-		    }
-
-		    while (queue.Count > 0) {
-			    var msg = queue.Peek();
-			    throttle.dataAmount += (ulong)msg.Blob.uncompressedDataSize;
-			    if (throttle.dataAmount >= MaxBytesPerPeriod) {
-				    break;
-			    }
-			    var targetContext = msg.FromProtectedContext ? LuauContext.Protected : LuauContext.Game;
-			    broadcastFromClientAction?.Invoke((object) targetContext, (object)connId, msg.Blob);
-			    queue.Dequeue();
-		    }
-        }
-
         private void OnEnable() {
 		    NetworkCore.SetNet(this);
-	    }
+        }
 
 	    private void OnClientConnected(NetworkConnectionToClient conn) {
 		    _throttle[conn.connectionId] = new ThrottleTrack();
@@ -100,7 +46,6 @@ namespace Assets.Luau.Network {
 
 	    private void OnClientDisconnected(NetworkConnectionToClient conn) {
 		    _throttle.Remove(conn.connectionId);
-		    _throttleQueue.Remove(conn.connectionId);
 	    }
 
 	    public void OnStartServer() {
@@ -131,12 +76,17 @@ namespace Assets.Luau.Network {
 	    private void OnBroadcastFromClient(NetworkConnectionToClient conn, NetBroadcast msg) {
 		    // Runs on the server, when the client broadcasts a message
 		    if ((ulong)msg.Blob.uncompressedDataSize >= MaxBytesAtOnce) {
-			    Debug.LogWarning($"Dropping message from client connection {conn.connectionId} due to exceeding max data size");
+			    Debug.LogWarning($"Dropping message from client connection {conn.connectionId} due to exceeding max data size.");
 			    return;
 		    }
 		    
 		    var now = Time.realtimeSinceStartup;
-		    var throttle = _throttle[conn.connectionId];
+		    if (!_throttle.TryGetValue(conn.connectionId, out var throttle)) {
+			    // If we don't have a throttle key, that means the client isn't connected anymore and we are processing an old message.
+			    print("hiu");
+			    return;
+		    }
+		    
 		    if (now >= throttle.nextClear) {
 			    throttle.dataAmount = 0;
 			    throttle.nextClear = now + ThrottleResetPeriod;
@@ -144,21 +94,12 @@ namespace Assets.Luau.Network {
 
 		    throttle.dataAmount += (ulong)msg.Blob.uncompressedDataSize;
 		    if (throttle.dataAmount >= MaxBytesPerPeriod) {
-			    // Queue the message (or drop it if the queue is full)
-			    if (!_throttleQueue.TryGetValue(conn.connectionId, out var queue)) {
-				    queue = new Queue<NetBroadcast>();
-				    _throttleQueue.Add(conn.connectionId, queue);
-			    }
-
-			    if (queue.Count >= MaxQueueSize) {
-				    Debug.LogWarning($"Dropping message from client connection {conn.connectionId} due to throttling");
-			    } else {
-				    queue.Enqueue(msg);
-			    }
-
+			    Debug.LogWarning(
+				    $"Disconnecting connection {conn.connectionId} because it's sending too much data. Data total {throttle.dataAmount} > {MaxBytesPerPeriod}");
+			    conn.Disconnect();
 			    return;
 		    }
-		    
+
 		    var targetContext = msg.FromProtectedContext ? LuauContext.Protected : LuauContext.Game;
 		    broadcastFromClientAction?.Invoke((object) targetContext, (object)conn.connectionId, msg.Blob);
 		}
