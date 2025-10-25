1ce63b335ab12888646d162c6d3da62e78f7e857
liamlutton <lutton+gh@me.com>
2025-10-25 11:05:05 -0700

    Update ApplicationAPI.cs

--- Runtime/Code/LuauAPI/ApplicationAPI.cs
+++ Runtime/Code/LuauAPI/ApplicationAPI.cs
@@ -1,11 +1,22 @@
 using System;
 using UnityEngine;
 
-[LuauAPI(ContextOverrideList = new [] {"OpenURL"}, ContextOverrideMask = (int) LuauContext.Protected)]
-public class ApplicationAPI : BaseLuaAPIClass
-{
-    public override Type GetAPIType()
-    {
+[LuauAPI(ContextOverrideList = new [] {
+    // Protected methods
+    "OpenURL",
+    "RequestUserAuthorization",
+    "RequestAdvertisingIdentifierAsync",
+    "CaptureScreenshot",
+    "Unload",
+    
+    // Protected file path members
+    "persistentDataPath",
+    "dataPath",
+    "temporaryCachePath",
+    "consoleLogPath",
+}, ContextOverrideMask = (int) LuauContext.Protected)]
+public class ApplicationAPI : BaseLuaAPIClass {
+    public override Type GetAPIType() {
         return typeof(Application);
     }
 }
