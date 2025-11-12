39542eacd51abaa887926ad98d33054c961dad1f
liamlutton <lutton+gh@me.com>
2025-11-12 14:01:39 -0800

    Swap to use TypeCache

--- Editor/Publish/Callback/IBuildAirshipGameBundle.cs
+++ Editor/Publish/Callback/IBuildAirshipGameBundle.cs
@@ -36,22 +36,13 @@ namespace Editor.Publish.Callback {
         /// Create instances of each implementation of IBuildAirshipGameBundle
         /// </summary>
         private static List<IBuildAirshipGameBundle> GetCallbackInstances() {
-            var callbackType = typeof(IBuildAirshipGameBundle);
             var instances = new List<IBuildAirshipGameBundle>();
-            
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            foreach (var assembly in assemblies) {
-                var types = assembly.GetTypes()
-                    .Where(t => callbackType.IsAssignableFrom(t) && 
-                                t.IsClass && 
-                                !t.IsAbstract);
-                
-                foreach (var type in types) {
-                    var instance = (IBuildAirshipGameBundle) Activator.CreateInstance(type);
-                    instances.Add(instance);
-                }
+
+            var derivedTypes = TypeCache.GetTypesDerivedFrom<IBuildAirshipGameBundle>();
+            foreach (var type in derivedTypes) {
+                var instance = (IBuildAirshipGameBundle) Activator.CreateInstance(type);
+                instances.Add(instance);   
             }
-            
             return instances.OrderBy(i => i.callbackOrder).ToList();
         }
     }
