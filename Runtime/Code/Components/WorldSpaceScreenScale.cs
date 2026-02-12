8debc42bbbfbf784db2082334b1c8414909f8cd0
Luke Chatton <chattonluke@gmail.com>
2026-02-12 10:16:37 -0800

    don't need to update scale every frame

--- Runtime/Code/Components/WorldSpaceScreenScale.cs
+++ Runtime/Code/Components/WorldSpaceScreenScale.cs
@@ -1,12 +1,17 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(RectTransform))]
 public class WorldSpaceScreenScale : MonoBehaviour
 {
     [SerializeField] private int scale = 1;
+    [NonSerialized] private float updateIntervalSeconds = 0.05f;
+    [NonSerialized] private float minScaleDelta = 0.0025f;
     private Vector3 defaultScale;
     private Camera cam;
     private RectTransform rect;
+    private float nextUpdateTime;
+    private float lastAppliedScale = -1f;
 
     // Start is called before the first frame update
     void Start() {
@@ -21,7 +26,22 @@ public class WorldSpaceScreenScale : MonoBehaviour
     
     // Update is called once per frame
     void Update() {
-        float dist = (cam.transform.position - transform.position).magnitude;
-        rect.transform.localScale = new Vector3((1 + (dist / 100) * scale) * defaultScale.x, (1 + (dist / 100) * scale) * defaultScale.y, 1 * defaultScale.z);
+        if (Time.unscaledTime < nextUpdateTime) {
+            return;
+        }
+        nextUpdateTime = Time.unscaledTime + updateIntervalSeconds;
+
+        float dist = Vector3.Distance(cam.transform.position, transform.position);
+        float targetScale = 1 + (dist / 100f) * scale;
+        if (Mathf.Abs(lastAppliedScale - targetScale) < minScaleDelta) {
+            return;
+        }
+
+        lastAppliedScale = targetScale;
+        rect.transform.localScale = new Vector3(
+            targetScale * defaultScale.x,
+            targetScale * defaultScale.y,
+            defaultScale.z
+        );
     }
 }
