17164b6bb82a646494280efd2a7102773f3e1400
Luke Chatton <chattonluke@gmail.com>
2026-02-25 15:28:07 -0800

    add log for downloading bundle

--- Runtime/Code/Bootstrap/BundleDownloader.cs
+++ Runtime/Code/Bootstrap/BundleDownloader.cs
@@ -122,7 +122,7 @@ public class BundleDownloader : Singleton<BundleDownloader> {
 				var request = UnityWebRequestProxyHelper.ApplyProxySettings(new UnityWebRequest(remoteBundleFile.Url));
 				var package = GetBundleFromId(remoteBundleFile.BundleId);
 				string path = Path.Combine(package.GetPersistentDataDirectory(platform), remoteBundleFile.fileName);
-				// Debug.Log($"Downloading Airship Bundle {remoteBundleFile.BundleId}/{remoteBundleFile.fileName}. url={remoteBundleFile.Url}, downloadPath={path}");
+				Debug.Log($"Downloading Airship Bundle {remoteBundleFile.BundleId}/{remoteBundleFile.fileName}. url={remoteBundleFile.Url}, downloadPath={path}");
 
 				request.downloadHandler = new DownloadHandlerFile(path);
 
