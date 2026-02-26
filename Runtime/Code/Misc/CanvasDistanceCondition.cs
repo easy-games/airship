541e3f8dbe76843129cd6a0c08cb77e223ede223
liamlutton <lutton+gh@me.com>
2026-02-25 18:33:19 -0800

    Allow disabling of canvas distance condition

--- Runtime/Code/Misc/CanvasDistanceCondition.cs
+++ Runtime/Code/Misc/CanvasDistanceCondition.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 using Code.Managers;
 using UnityEngine;
 
 
 [LuauAPI]
 public class CanvasDistanceCondition : MonoBehaviour {
+    private HashSet<int> disablers = new HashSet<int>();
+    private int disablerId = 0;
+    
     public float maxDistance {
         get => _maxDistance;
         set {
@@ -19,5 +23,25 @@ public class CanvasDistanceCondition : MonoBehaviour {
 
     private void OnEnable() {
         CanvasDistanceManager.Instance.Register(this);
+    }
+
+    /// <summary>
+    /// Adds a disabler to this distance condition. While any disablers exist this condition won't run.
+    /// </summary>
+    /// <returns>
+    /// Id of disabler that can be passed to RemoveDisabler
+    /// </returns>
+    public int AddDisabler() {
+        disablerId++;
+        disablers.Add(disablerId);
+        return disablerId;
+    }
+
+    public void RemoveDisabler(int id) {
+        disablers.Remove(id);
+    }
+    
+    public bool IsConditionDisabled() {
+        return this.disablers.Count > 0;
     }
 }
